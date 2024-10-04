444aa0965f1fe2b038ebdbf3ffe0b93222a65711
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-10-04 18:35:35 +0530

    Time: 7 ms (21.21%), Space: 45.7 MB (29.2%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n = nums.length;
+        if(nums==null||nums.length==0){
+            return new int[0];
+        }
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        // As we are viewing No Data Modification.
+        // If you try to modify Errror, Try Iterator<Integer> it = hm.keySet().iterator();
+        for(int ikey : hm.keySet()){
+            if(hm.get(ikey)==1){
+                res.add(ikey);
+            }
+        }
+        int[] resArr = new int[res.size()];
+        for(int i=0;i<resArr.length;i++){
+            resArr[i]=res.get(i);
+        }
+        return resArr;        
+    }
+}
