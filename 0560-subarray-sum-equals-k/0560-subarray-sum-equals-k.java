a75260f09c89196efc55a43acba2a98e041c5278
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-10-24 23:00:27 +0530

    Time: 24 ms (76.3%), Space: 46.4 MB (54.99%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,21 +1,37 @@
 /**
-Sunday, June 9, 2024 2:17:34 PM
-Time Complexity: O(n^2)
-Space Complexity: O(1)
-Please Update Here, TimeComplexity and SpaceComplexity of the Code. Explain in Detail.
+Thursday, October 24, 2024 10:22:12 PM
+Finding TimeComplexity Formula : No of Function Call * TC per Call
+
+Verify it, Correct it. Explain it as well.
+BRUTEFORCEAPPROACH | OPTIMALAPPROACH | SPACEOPTIMIZIEDAPPROACH
+
+Thinking of HashSet NO 
+Think of HashMap - We need to return the Frequency of Occurence.
+
+[-1,-1,1]  , K=0 
+
+n<=0 || k<=0 return 0; // This is WRONG, Applying Constraint on k is WRONG
+
+T.C - O(n)
+S.C - O(n)
 */
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {                
-                    sum += nums[j];                    
-                if (sum == k){
-                    count++;
-                    }
-                }
+        int n = nums.length;
+        if(n<=0) return 0; 
+              
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,1); 
+        int csum = 0;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            csum +=nums[i];
+            if(hm.containsKey(csum-k)){
+                cnt+=hm.get(csum-k);
             }
-        return count;
-    }	
+            hm.put(csum,hm.getOrDefault(csum,0)+1);
+        }
+        return cnt;
+    }
 }
