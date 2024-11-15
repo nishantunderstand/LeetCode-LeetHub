6755d002f3bcf910fb6783e86d52e19bc921f300
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-11-15 12:37:58 +0530

    Time: 32 ms (5.83%), Space: 45 MB (5.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+/**
+Friday, November 15, 2024 12:17:50 PM
+T.C - O(n^2)
+S.C - O(1)
+*/
+
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<=0) return false;
+        boolean changed = true;
+        while(changed){
+            int lengthBefore = s.length();
+            s = s.replace("()","") // O(n)
+                .replace("{}","")
+                .replace("[]","");
+
+            int lengthAfter = s.length();
+            if(lengthBefore == lengthAfter){
+                changed = false;
+            }
+        }
+        return s.isEmpty();
+    }
+}
