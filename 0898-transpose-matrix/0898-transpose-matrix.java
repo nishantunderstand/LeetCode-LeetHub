3a4f25079768eee8453416ff7e9add940815c496
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-11-15 20:58:53 +0530

    Time: 0 ms (100%), Space: 44.8 MB (57.54%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.java
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -1,7 +1,15 @@
+/**
+Friday, November 15, 2024 8:56:58 PM
+T.C - O(row*col)
+S.C - O(row*col)
+*/
+
+
 class Solution {
     public int[][] transpose(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
+        if(matrix==null || row==0||col==0) return new int[0][0];
         int[][] res = new int[col][row];
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
