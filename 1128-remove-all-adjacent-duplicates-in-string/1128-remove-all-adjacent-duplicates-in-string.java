0c8ff2b978df817d14926965867a50bb21c98300
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-11-18 12:57:33 +0530

    Time: 17 ms (68.47%), Space: 45.1 MB (84.47%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -11,9 +11,57 @@ st.peek()==ch && !st.isEmpty() WRONG ORDER
 
 class Solution {
     public String removeDuplicates(String s) {
-        ArrayDeque<Character> st = new ArrayDeque<>();
-  
-        for(int i=0;i<s.length();i++){
+        if(s.length()<=0) return "";
+        //return stackApproach(s);
+        //return stackApproach2(s);
+        return stackApproach3Better(s);
+    }
+
+
+    // Monday, November 18, 2024 12:26:34 PM
+    // T.C - O(n+n+n)=O(n) | S.C - O(n+n)
+    public String stackApproach3Better(String s){
+        ArrayDeque<Character> st = new ArrayDeque<>(); //O(n)
+        for(int i=s.length()-1;i>=0;i--){ // O(n)
+            Character ch = s.charAt(i);
+            if(!st.isEmpty() && st.peek()==ch ){
+                st.pop();
+            }else{
+                st.push(ch);
+            }                                    
+        }
+        StringBuilder sb = new StringBuilder(); // O(n)
+        while(!st.isEmpty()){ // O(n)
+             sb.append(st.pop());
+        }
+        return sb.toString(); // O(n)        
+    }
+    
+    // Monday, November 18, 2024 12:26:34 PM
+    // T.C - O(n+n+n^2)=O(n^2) | S.C - O(n+n)
+    public String stackApproach2(String s){
+        ArrayDeque<Character> st = new ArrayDeque<>(); //O(n)
+        for(int i=0;i<s.length();i++){ // O(n)
+            Character ch = s.charAt(i);
+            if(!st.isEmpty() && st.peek()==ch ){
+                st.pop();
+            }else{
+                st.push(ch);
+            }                                    
+        }
+        StringBuilder sb = new StringBuilder(); // O(n)
+        while(!st.isEmpty()){ // O(n^2) // This Approach is not Correct or Recommended
+            sb.insert(0,st.pop());
+        }
+        return sb.toString(); // O(n)        
+    }
+
+
+    // Monday, November 18, 2024 12:26:34 PM
+    // T.C - O(n+n+n) | S.C - O(n+n)
+    public String stackApproach(String s){
+        ArrayDeque<Character> st = new ArrayDeque<>(); //O(n)
+        for(int i=0;i<s.length();i++){ // O(n)
             Character ch = s.charAt(i);
             if(!st.isEmpty() && st.peek()==ch ){
                 st.pop();
@@ -22,11 +70,11 @@ class Solution {
             }
             
         }
-
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
+        StringBuilder sb = new StringBuilder(); // O(n)
+        while(!st.isEmpty()){ // O(n)
             sb.append(st.pop());
         }
-        return sb.reverse().toString();
+        return sb.reverse().toString(); // O(n) + O(n)
+        // TC From StringBuilder to String 
     }
 }
