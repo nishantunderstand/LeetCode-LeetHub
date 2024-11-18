3075ac9be055f6764dcae3ea169d4033de96ae97
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-11-18 20:38:04 +0530

    Time: 1 ms (86.58%), Space: 42.2 MB (17.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,34 +1,27 @@
-/**
-Tuesday, October 1, 2024 11:31:57 PM
-T.C - O(n^2)
-S.C - O(n^2)
-*/
-
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> al = new ArrayList<>();
-        if(numRows<=0){
-            return al;
-        }
-        
-        if(numRows==1){
-            return Arrays.asList(Arrays.asList(1));
-        }
+        return bruteForce(numRows);
+    }
+    // Monday, November 18, 2024 8:36:36 PM
+    // T.C - O(n^2) | S.C - O(n)
+    public List<List<Integer>> bruteForce(int numRows){        
+        List<List<Integer>> res = new ArrayList<>();
+        if(numRows<=0)return res;        
+        if(numRows==1)return Arrays.asList(Arrays.asList(1));
         
         int row = numRows;
         for(int i=0;i<row;i++){
-            List<Integer> res = new ArrayList<>();
+            List<Integer> curr = new ArrayList<>();
             for(int j=0;j<=i;j++){
                 if(j==i||j==0){
-                    res.add(1);
+                    curr.add(1);
                 }else{
-                    int val = al.get(i-1).get(j-1) + al.get(i-1).get(j);
-                    res.add(val);
+                    int val = res.get(i-1).get(j-1) + res.get(i-1).get(j);
+                    curr.add(val);
                 }
             }
-            al.add(res);
+            res.add(curr);
         }
-        return al;
+        return res;
     }
 }
