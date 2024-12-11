8ca5a3a50323a87ecbf60441ba2fced26af7eb51
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-11 22:41:16 +0530

    Time: 1101 ms (10.33%), Space: 46.9 MB (20.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,36 +1,24 @@
 /**
-Thursday, October 24, 2024 10:22:12 PM
-Finding TimeComplexity Formula : No of Function Call * TC per Call
-
-Verify it, Correct it. Explain it as well.
-BRUTEFORCEAPPROACH | OPTIMALAPPROACH | SPACEOPTIMIZIEDAPPROACH
-
-Thinking of HashSet NO 
-Think of HashMap - We need to return the Frequency of Occurence.
-
-[-1,-1,1]  , K=0 
-
-n<=0 || k<=0 return 0; // This is WRONG, Applying Constraint on k is WRONG
-
-T.C - O(n)
-S.C - O(n)
+- Sum Checking should be done inside the Class.
+- Regarding SubArray, It need to be continous subArray
 */
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        if(n<=0) return 0; 
-              
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        hm.put(0,1); 
-        int csum = 0;
+        return bruteForceApproach(nums, k);
+    }
+
+    // Wednesday, December 11, 2024 9:46:20 PM
+    // T.C - O(n^2) | S.C - O(1)
+    public int bruteForceApproach(int[] nums, int k) {
         int cnt = 0;
-        for(int i=0;i<n;i++){
-            csum +=nums[i];
-            if(hm.containsKey(csum-k)){
-                cnt+=hm.get(csum-k);
+        for (int i = 0; i < nums.length; i++) {
+            int csum = 0;
+            for (int j = i; j < nums.length; j++) {
+                csum += nums[j];
+                if (csum == k) { 
+                    cnt++;
+                }
             }
-            hm.put(csum,hm.getOrDefault(csum,0)+1);
         }
         return cnt;
     }
