028a865ea2a5293637627d4e5b1fdf09a2d1daba
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-12 17:46:25 +0530

    Time: 1 ms (86.79%), Space: 41.2 MB (18.34%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,16 +1,11 @@
-/**
-Sunday, November 17, 2024 3:07:00 PM
-T.C - O(n^2/n/logn/1√n) | S.C - O(n/1)
-Verify it, Correct it. Explain it as well.
-*/
-
 class Solution {
     public int mySqrt(int x) {
-        if(x<=0) return 0;
-        if(x==1) return x;
         //return bruteForceApproach(x);
-        return iterativeApproach(x);
+        //return iterativeApproach(x);
+        return binarySearchApproach(x);
+  
     }
+    
     // T.C - O(1) | S.C - O(1)
     public int bruteForceApproach(int x){
         return (int) Math.sqrt(x);
@@ -26,5 +21,27 @@ class Solution {
         }
         return potRoot;
     }
-    // You are not Handling Integer Overflow Properly root*root<=x
+
+    // T.C - O(logn) | S.C - O(1)
+    public int binarySearchApproach(int x){
+        if(x<=0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+        int lo=1;
+        int hi=x;
+        int ans=1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
 }
