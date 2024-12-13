79dd82f32a72d90ad84a47aa9bd17de9bfab137a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-13 19:57:22 +0530

    Time: 0 ms (100%), Space: 40.8 MB (42.92%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        return bruteForceConvertToTitle(columnNumber);
+    }
+
+    // Friday, December 13, 2024 7:51:42 PM
+    // T.C - O(logn26) | S.C - O(1)
+    public String bruteForceConvertToTitle(int columnNumber) {
+        StringBuilder res = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            char ch = (char) ('A' + (columnNumber % 26));
+            res.append(ch);
+            columnNumber /= 26;
+        }
+        return res.reverse().toString();
+    }
+}
