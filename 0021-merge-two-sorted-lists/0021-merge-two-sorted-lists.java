30609e4c5e17fd185620b007035ad83984eee808
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-14 18:23:06 +0530

    Time: 0 ms (100%), Space: 42.3 MB (68.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,33 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return approach1(list1, list2);
+        return bruteForceApproach(list1,list2);
     }
 
-    public ListNode approach1(ListNode list1, ListNode list2) {
-        // Create a dummy node to simplify list operations
-        ListNode dummy = new ListNode();
-        ListNode res = dummy;
-
-        // Merge the two lists until one of them is exhausted
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                res.next = list1; // Attach list1 node to result
-                list1 = list1.next; // Move to the next node in list1
-            } else {
-                res.next = list2; // Attach list2 node to result
-                list2 = list2.next; // Move to the next node in list2
+    public ListNode bruteForceApproach(ListNode list1, ListNode list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode dummyNode = new ListNode(-1);
+        ListNode current = dummyNode;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                current.next=list1; 
+                list1=list1.next;           
+            }else{
+                current.next=list2;
+                list2=list2.next;
             }
-            res = res.next; // Move the result pointer
+            current=current.next;
         }
-
-        // Append the remaining nodes of list1 or list2, if any
-        if (list1 != null) {
-            res.next = list1; // List1 has remaining nodes
-        } else if (list2 != null) {
-            res.next = list2; // List2 has remaining nodes
+        if(list1!=null){
+            current.next=list1;
         }
-
-        // Return the merged list, skipping the dummy node
-        return dummy.next;
+        if(list2!=null){
+            current.next=list2;
+        }
+        return dummyNode.next;
     }
 }
