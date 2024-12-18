f459070115961374a9180156d7c3e2da6c5b1e1c
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-18 12:41:14 +0530

    Time: 16 ms (94.86%), Space: 55 MB (40.65%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -0,0 +1,95 @@
+// LeetCode 881 , Children and Rides Question 
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        return twoPointerEqualVersion(people, limit);
+        // return twoPointer(people,limit);
+        // return twoPointerMod(people, limit);
+        // return twoPointerPepCoding(people, limit);
+
+    }
+
+    public int twoPointerPepCoding(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boat = 0;
+        while (left <= right) {
+            int target = people[left] + people[right];
+            if (target <= limit) {
+                left++;
+                right--;
+                boat++;
+            } else {
+                right--;
+                boat++;
+            }
+        }
+        return boat;
+    }
+
+    // Failed
+    public int twoPointerEqualVersion(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int target = 0;
+        int boat = 0;
+        while (left < right) {
+            target = people[left] + people[right];
+            if (target <= limit) {
+                left++;
+                right--;
+                boat++;
+            } else {
+                right--;
+                boat++;
+            }
+        }
+
+        if (left == right) {
+            boat++;
+        }
+        return boat;
+
+    }
+
+    // What wrong here ? // FAILED
+    public int twoPointer(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int target = 0;
+        int boat = 0;
+        while (left <= right) {
+            target = people[left] + people[right];
+            if (target <= limit) {
+                left++;
+                right--;
+                boat++;
+            } else {
+                right--;
+                boat++;
+            }
+        }
+        return boat;
+    }
+
+    // TC - O(nlogn)+O(n) | SC-O(1) || Correct But I don't like Code Structure
+    public int twoPointerMod(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int target = 0;
+        int boat = 0;
+        while (left <= right) {
+            target = people[left] + people[right];
+            if (target <= limit) {
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+
+}
