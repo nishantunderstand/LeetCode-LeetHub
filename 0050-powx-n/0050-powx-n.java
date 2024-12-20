f947071cfc7877af375f3f26de627c8ba8c6bcdb
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-20 15:09:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (6.9%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,36 +1,61 @@
-
 class Solution {
     public double myPow(double x, int n) {
-        return bruteForce(x, n);
-        // return iterative(x, n);
-        // return recursive(x, n); /
-        // return recursiveMod(x, n);
-        // return recursiveFastExponent(x, n);
+        // return bruteForce(x, n);
+        // return iterative(x, n); // Pending
+        // return recursive(x, n); // TLE
+        // return recursiveMod(x, n); // RunTime Error
+        // return recursiveModAdv(x, n); // RunTime Error
+        return recursiveFinal(x, n);
     }
 
-    public double recursiveFastExponent(double x, int n) {
+    public double recursiveFinal(double x, int n) {
         if (n == 0) {
             return 1.0;
         }
+
         if (n < 0) {
+            if(n==Integer.MIN_VALUE){
+                return x* 1/recursiveFinal(x,Integer.MAX_VALUE);
+            }
             x = 1 / x;
             n = -n;
         }
-        double half = 0.0;
-        double halfSquared = 0.0;
+        double half = recursiveMod(x, n / 2);
         if (n % 2 == 0) {
-            half = recursiveFastExponent(x, n / 2);
-            halfSquared = half * half;
-            return halfSquared;
+            return half * half;
         } else {
-            return halfSquared * half;
+            return half * half * x;
         }
     }
 
+    // RunTime Error
+    public double recursiveModAdv(double x, int n) {
+        long N = (long) n;
+        if (N == 0) {
+            return 1.0;
+        }
+        if (x == 0) {
+            return 0.0;
+        }
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double half = recursiveModAdv(x, (int) N / 2);
+        if (N % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
+    // RunTime Error
     public double recursiveMod(double x, int n) {
         if (n == 0) {
             return 1.0;
         }
+
         if (n < 0) {
             x = 1 / x;
             n = -n;
@@ -43,21 +68,23 @@ class Solution {
         }
     }
 
+    // TLE
     public double recursive(double x, int n) {
-      if (n == 0) {
-      return 1.0;
-      }
-      if (n < 0) {
-      x = 1 / x;
-      n = -n;
-      }
-      if (n % 2 == 0) {
-      return recursive(x, n / 2)*recursive(x, n / 2);
-      } else {
-      return recursive(x, n / 2)*recursive(x, n / 2)* x;
-      }
-      }
+        if (n == 0) {
+            return 1.0;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        if (n % 2 == 0) {
+            return recursive(x, n / 2) * recursive(x, n / 2);
+        } else {
+            return recursive(x, n / 2) * recursive(x, n / 2) * x;
+        }
+    }
 
+    // IDK
     public double iterative(double x, int n) {
         if (n % 2 == 0) {
 
