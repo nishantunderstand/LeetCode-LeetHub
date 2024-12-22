a10cb74928ca4dba1a1e4b4d668884d7b5ed4b6b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2024-12-22 16:09:00 +0530

    Time: 71 ms (20.35%), Space: 58.7 MB (41.06%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,6 +1,25 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return arrayApproach(nums,k);
+        // return arrayApproach(nums,k);
+        return maxHeapApproach(nums,k);
+    }
+
+    // Sunday, December 22, 2024 4:07:12 PM
+    // Time Complexity:O(nlogn) | Space Complexity:O(n)
+    public int maxHeapApproach(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        // Time Complexity:O(nlogn) | Space Complexity:O(1)
+        for(int i:nums){
+            maxHeap.offer(i);
+        }
+        int res=0;
+        // Time Complexity:O(nlogn) | Space Complexity:O(1)
+        while(k>0){
+            res=maxHeap.poll();
+            k--;
+        }
+        return res;
     }
 
     // Sunday, December 22, 2024 4:02:40 PM
