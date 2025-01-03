2f3d678bd82a16fb9efab1d3f455003db7842339
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-03 11:07:15 +0530

    Time: 5 ms (74.44%), Space: 58.1 MB (28.82%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxArea(int[] height) {
+        //return bruteForce(height);
+        return twoPointer(height);
+    }
+
+    public int twoPointer(int[] height) {
+        
+        int mArea=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right){
+            int wid = right-left;
+            int heig = Math.min(height[left],height[right]);
+            int cArea = wid*heig;
+            mArea = Math.max(cArea,mArea);
+            if(height[left]<height[right]) left++;
+            else right--;
+        }
+        return mArea;
+    }
+
+    // Friday, January 3, 2025 11:02:15 AM
+    // Time Complexity:O(n^2) | Space Complexity:O(1)
+    // TLE
+    public int bruteForce(int[] height) {
+        int maxArea=0;
+        if(height==null || height.length==0) return maxArea;
+        for(int i=0;i<height.length;i++){
+            for(int j=i+1;j<height.length;j++){
+                int w = j-i;
+                int h = Math.min(height[i],height[j]);
+                int cArea = h*w;
+                maxArea=Math.max(cArea,maxArea);
+            }
+        }
+        return maxArea;
+    }
+}
+
+/**
+LeetCode 11
+global maxArea
+NextGreaterElement_Left And Right
+Difference Height * Width (Index Difference B/W)
+currArea
+Find the Maximium of it.
+*/
