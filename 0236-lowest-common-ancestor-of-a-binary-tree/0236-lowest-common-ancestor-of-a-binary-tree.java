847ed27b879f8cf2a8e534dcae0f6fd7aa6e2d3b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-04 18:09:58 +0530

    Time: 7 ms (66.03%), Space: 45 MB (24.65%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,17 +9,30 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return bruteForce(root,p,q);
+        //return bruteForce(root,p,q);
+        return bruteForceBetter(root,p,q);
+    }
+    
+    // Cleaner and much better
+    public TreeNode bruteForceBetter(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root==p||root==q) return root;     
+        TreeNode left = bruteForceBetter(root.left,p,q);
+        TreeNode right = bruteForceBetter(root.right,p,q);        
+        if(left!=null && right!=null){
+            return root;
+        }
+        return left!=null?left:right;
     }
 
+
+
+    // Saturday, January 4, 2025 5:57:48 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
     public TreeNode bruteForce(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return root;
-
-        if(root.val==p.val||root.val==q.val) return root;
-        
+        if(root.val==p.val||root.val==q.val) return root;        
         TreeNode left = bruteForce(root.left,p,q);
-        TreeNode right = bruteForce(root.right,p,q);
-        
+        TreeNode right = bruteForce(root.right,p,q);        
         if(left!=null && right!=null){
             return root;
         }else if(left!=null){
