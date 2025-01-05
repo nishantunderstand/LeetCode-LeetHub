4c04ff02408258fc891bb9d29cfcb1691a323716
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-05 20:52:27 +0530

    Time: 1 ms (47.22%), Space: 41.4 MB (11.47%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -3,11 +3,15 @@ class Solution {
         return recursiveApproach(num);
         //return directFormula(num);
     }
+
+    // Sunday, January 5, 2025 8:51:12 PM
+    // Time Complexity:O(1) | Space Complexity:O(1)
     public int directFormula(int num) {
         if(num==0) return 0;
         return num%9==0?9:num%9;
     }
-
+    // Sunday, January 5, 2025 8:51:17 PM
+    // Time Complexity:O(logn) | Space Complexity:O(logn)
     public int recursiveApproach(int num) {
         if(num<10) return num;
         int digitSum=0;
