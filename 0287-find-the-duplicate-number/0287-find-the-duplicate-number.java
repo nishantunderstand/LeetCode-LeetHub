604fe7219e69ac08310de9169dd5057a068b2f25
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-16 16:27:59 +0530

    Time: 19 ms (35.91%), Space: 57.2 MB (90.06%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,24 @@
+/**
+Here I need to return the Element , Not the Index
+1. Sorting , then compare
+    Then I need to Duplicate Case 
+2. HashMap , value count >1 , Much Better
+    You cannot use 2 Pass - NO
+    You need to solve in 1 Pass
+
+Floyd's Tortoise and Hare algorithm
+*/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(Integer num : nums){
+            if(hm.containsKey(num)){
+                return num;
+            }
+            hm.put(num,1); // Why are we not checking getOrDefault ?
+        }
+        return -1;         
+    }
+}
