09de97a8d5226fc4da59afc984a18731e6d75580
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-19 19:09:40 +0530

    Time: 147 ms (49.21%), Space: 174.1 MB (43.3%) - LeetHub

---
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        // Bi-directional Graph
+        for(int i=0;i<edges.length;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+        boolean[] visited = new boolean[n+1];
+        return hasPath(adjList,visited,source,destination);
+    }
+    public boolean hasPath(List<List<Integer>> graphs,boolean[] visited, int src, int dest) {
+        if(src==dest) return true;
+        if(visited[src]) return false;
+        visited[src]=true;
+        for(int nei : graphs.get(src)){
+            if(hasPath(graphs,visited,nei,dest)) return true;
+        }
+        return false;
+    }
+}
