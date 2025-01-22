1f9a33b9cc5a5b08c445e87f15f5117b52a9f0c8
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-22 23:53:29 +0530

    Time: 0 ms (100%), Space: 46.2 MB (16.53%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        // Am i applying correct Edge cases or not ?
+        if(nums==null||target<0){
+            return new int[]{-1,-1};
+        }
+        // Finding 1st
+        int first = -1;
+        int lo = 0;
+        int hi = nums.length-1;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                first = mid;
+                hi = mid-1;
+            }else if(nums[mid]>target){
+                hi = mid-1;
+            }else{
+                lo = mid+1;
+            }
+        }
+        int last = -1;
+        lo = 0;
+        hi = nums.length-1;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                last = mid;
+                lo = mid+1;
+            }else if(nums[mid]>target){
+                hi = mid-1;
+            }else{
+                lo = mid+1;
+            }
+        }
+        return new int[]{first,last};        
+        
+    }
+}
