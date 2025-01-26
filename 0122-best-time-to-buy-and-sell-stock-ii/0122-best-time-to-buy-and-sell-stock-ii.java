c1f1849dfac43a9b5e66ee830dcc8a5595a1f117
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-26 21:44:24 +0530

    Time: 0 ms (100%), Space: 45.2 MB (99.58%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        //return backtrackingApproach(prices);
+        return greedyApproach(prices);
+    }
+
+    /**
+    // Left To Right
+    public int backtrackingApproach(int[] prices) {
+        return backtrackingHelper(prices,0);
+    }
+    
+    public int backtrackingHelper(int[] prices,int idx) {
+        if(idx>prices.length) return 0;        
+        int notpick = backtrackingApproach(prices,idx+1);        
+        // Sell
+        // Buy 
+        int pick = prices[idx] +backtrackingApproach(prices,idx+1); 
+        // I need to add Difference, Not prices, This is a different Question
+        return 0;
+    }
+    */
+
+
+    // Multiple Buy and Sell Possible
+    public int greedyApproach(int[] prices) {
+        int n = prices.length;
+        if(prices==null || n<2) return 0;
+        int profit = 0;
+        for(int i=1;i<n;i++){
+            if(prices[i-1]<prices[i]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
