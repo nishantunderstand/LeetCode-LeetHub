d3358f56d6397c081bfa372a957775a0a0ed18f5
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-26 14:00:41 +0530

    Time: 2 ms (75.82%), Space: 61.8 MB (40.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxPro=0;
-        int minPrice=Integer.MAX_VALUE;
-        for(int i=0;i<prices.length;i++){
-            minPrice=Math.min(prices[i],minPrice);
-            maxPro = Math.max(maxPro,prices[i]-minPrice);
+        return bruetForce(prices);
+
+    }
+    
+    // Sunday, January 26, 2025 1:57:19 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int bruetForce(int[] prices) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(prices[i], minPrice);
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
         }
         return maxPro;
     }
