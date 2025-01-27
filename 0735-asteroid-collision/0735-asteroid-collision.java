88c91c7f3181ca4e2eee8c1c833ab4f60ad0bdec
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-27 23:05:24 +0530

    Time: 7 ms (47.78%), Space: 45.3 MB (70.28%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int n = asteroids.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<n;i++){
+            int elem = asteroids[i];            
+            if(elem>0){
+                st.push(elem);
+            }else{
+                // Top Element is Postive and Bigger, Pop Till it reached Zero
+                while(!st.isEmpty() && st.peek()>0 && Math.abs(elem)>st.peek()){
+                    st.pop();
+                }
+                // If the stack is empty or the top is negative, push the current asteroid
+                if(st.isEmpty() || st.peek()<0){
+                    st.push(elem);
+                }
+                else if(st.peek() + elem ==0) st.pop();
+            }
+        }
+        
+        // Converting to Array 
+        int[] res = new int[st.size()];
+        int len = res.length-1;
+        
+        for(int i=len;i>=0;i--){
+            res[i]=st.pop();
+        }
+        return res;
+    }
+}
+
+/**
+[-5,-5,-5]
+How this is Handled ?
+*/
