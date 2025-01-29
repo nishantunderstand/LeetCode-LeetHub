24071d54f2613ef0bb13b59b84fec8fed77972a4
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-29 20:32:19 +0530

    Time: 0 ms (100%), Space: 41.5 MB (81%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,34 +1,40 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+        return recursiveApproach(root);
+    }
+    // Wednesday, January 29, 2025 8:31:01 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
+    public List<Integer> recursiveApproach(TreeNode root) {
         List<Integer> al = new ArrayList<>();
-        if(root == null){
+        if (root == null) {
             return al;
         }
-        postorderTraversalHelper(root,al);
+        postorderTraversalHelper(root, al);
         return al;
     }
+
     // LRD
-    private void postorderTraversalHelper(TreeNode root,List<Integer> al){
-        if(root == null){
+    private void postorderTraversalHelper(TreeNode root, List<Integer> al) {
+        if (root == null) {
             return;
         }
-        postorderTraversalHelper(root.left,al);
-        postorderTraversalHelper(root.right,al);
+        postorderTraversalHelper(root.left, al);
+        postorderTraversalHelper(root.right, al);
         al.add(root.val);
     }
 }
