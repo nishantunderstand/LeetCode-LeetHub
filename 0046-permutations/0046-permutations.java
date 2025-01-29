d34e4b0ec795e347c7bb1d098de20af1378f766f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-01-29 18:42:08 +0530

    Time: 1 ms (93.15%), Space: 44.3 MB (91.25%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        return backtrackingApproach(nums);
+    }
+
+    // Wednesday, January 29, 2025 6:41:02 PM
+    // Time Complexity:O(n!) | Space Complexity:O(n!)
+    public List<List<Integer>> backtrackingApproach(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> inner = new ArrayList<>();
         boolean[] vis = new boolean[nums.length];
-        helper(res,inner,nums,vis);
+        helper(res, inner, nums, vis);
         return res;
     }
 
-    private void helper(List<List<Integer>> res,List<Integer> inner,int[] nums,boolean[] vis) {
-        if(inner.size()==nums.length){
+    public void helper(List<List<Integer>> res, List<Integer> inner, int[] nums, boolean[] vis) {
+        if (inner.size() == nums.length) {
             res.add(new ArrayList<>(inner));
             return;
         }
-    
-        for(int i=0;i<nums.length;i++){
-            if(vis[i]) continue;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (vis[i])
+                continue;
             inner.add(nums[i]);
-            vis[i]=true;            
-            helper(res,inner,nums,vis);
-            inner.remove(inner.size()-1);
-            vis[i]=false;
+            vis[i] = true;
+            helper(res, inner, nums, vis);
+            inner.remove(inner.size() - 1);
+            vis[i] = false;
         }
     }
 }
