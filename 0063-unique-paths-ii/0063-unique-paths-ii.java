0965a0f9a2ebbd7c190ddff5eee50b6b6984f40f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-01 20:59:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (19.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -6,18 +6,17 @@ class Solution {
         for(int[] rowArr : dp){
             Arrays.fill(rowArr,-1);
         }
-        int cnt = 0;
-        return dpTopDown(obstacleGrid,row-1,col-1,dp,cnt); //Pending
+        return dpTopDown(obstacleGrid,row-1,col-1,dp); //Pending
         //return dpBottomUp(,dp); //Pending
         //return dpSpace(); //Pending
         //return prefixApproach(); //Pending
     }
-    public int dpTopDown(int[][] mat,int dr,int dc,int[][]dp,int cnt) {
+    public int dpTopDown(int[][] mat,int dr,int dc,int[][]dp) {
         if(dr<0 || dc< 0 || mat[dr][dc]==1) return 0;        
         if(dr==0 && dc==0) return 1;
         if(dp[dr][dc]!=-1) return dp[dr][dc];
-        int up = dpTopDown(mat,dr-1,dc,dp,cnt+1);
-        int left = dpTopDown(mat,dr,dc-1,dp,cnt+1);
+        int up = dpTopDown(mat,dr-1,dc,dp);
+        int left = dpTopDown(mat,dr,dc-1,dp);
         dp[dr][dc]=up+left;
         return dp[dr][dc];
     }
