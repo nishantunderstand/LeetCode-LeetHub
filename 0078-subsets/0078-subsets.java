37bd0343330431500f2bf468182e85e4f59cbb60
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-02 13:56:56 +0530

    Time: 1 ms (54.45%), Space: 42.8 MB (47.05%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,9 +2,24 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        backtracking(0,nums,res,curr);
+        //backtracking(0,nums,res,curr);
+        backtrackingForLoop(0,nums,res,curr);
         return res;
     }
+
+    // Sunday, February 2, 2025 1:55:45 PM
+    // Time Complexity:O(2^n) | Space Complexity:O(n)
+    public void backtrackingForLoop(int idx,int[] nums,List<List<Integer>> res,List<Integer> curr){
+        res.add(new ArrayList<>(curr));
+        for(int i=idx;i<nums.length;i++){
+            curr.add(nums[i]);
+            backtrackingForLoop(i+1,nums,res,curr); //<-- i is Used
+            curr.remove(curr.size()-1);
+        }        
+    }
+
+    // Sunday, February 2, 2025 1:55:42 PM
+    // Time Complexity:O(2^1) | Space Complexity:O(n)
     public void backtracking(int idx,int[] nums,List<List<Integer>> res,List<Integer> curr){
         if(idx==nums.length){
             res.add(new ArrayList<>(curr));
