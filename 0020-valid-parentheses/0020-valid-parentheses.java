ac85d6e6032912c4e356a890cd27b7a811d98342
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-06 16:12:27 +0530

    Time: 3 ms (42.1%), Space: 42.1 MB (22.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,79 @@
-/**
-Friday, November 15, 2024 12:17:50 PM
-T.C - O(n^2)
-S.C - O(1)
-*/
-
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()<=0) return false;
+        //return stackApproach(s);
+        // return bruteForce(s);
+        return hashingAndStack(s);
+    }
+
+    // Thursday, February 6, 2025 4:11:52 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public boolean hashingAndStack(String s) {
+        int len = s.length();
+        if(s==null || len==0 || len%2==1) return false;
+        
+        HashMap<Character,Character> hm = new HashMap<>();
+        hm.put(')','(');
+        hm.put('}','{');
+        hm.put(']','[');
+
+        ArrayDeque<Character> st = new ArrayDeque<>();
+        
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            if(hm.containsKey(curr)){
+                if(st.isEmpty() || st.peek()!=hm.get(curr)){
+                    return false;
+                }
+                st.pop();
+            }else{
+                st.push(curr);
+            }            
+        }
+        return st.isEmpty();
+    }
+
+    // Tuesday, December 17, 2024 8:56:49 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public boolean stackApproach(String s) {
+        int n = s.length();
+        if (s == null || n <= 0) {
+            return true; // Empty String is Valid
+        }
+        Deque<Character> st = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+            if (curr == '(' || curr == '{' || curr == '[') {
+                st.push(curr);
+            } else if (curr == ')' && !st.isEmpty() && st.peek() == '(') {
+                return true;
+            } else if (curr == '}' && !st.isEmpty() && st.peek() == '{') {
+                return true;
+            } else if (curr == ']' && !st.isEmpty() && st.peek() == '[') {
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return st.isEmpty() ? true : false;
+    }
+
+
+    /**
+     * Friday, November 15, 2024 12:17:50 PM
+     * T.C - O(n^2) | S.C - O(1)
+     */
+    public boolean bruteForce(String s) {
+        if (s.length() <= 0)
+            return false;
         boolean changed = true;
-        while(changed){
+        while (changed) {
             int lengthBefore = s.length();
-            s = s.replace("()","") // O(n)
-                .replace("{}","")
-                .replace("[]","");
+            s = s.replace("()", "") // O(n)
+                    .replace("{}", "")
+                    .replace("[]", "");
 
             int lengthAfter = s.length();
-            if(lengthBefore == lengthAfter){
+            if (lengthBefore == lengthAfter) {
                 changed = false;
             }
         }
