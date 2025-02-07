3de73373680103a77adfa360ed413edf7ebd9799
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-07 23:25:36 +0530

    Time: 4 ms (99.95%), Space: 47.5 MB (90.31%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,6 +1,27 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        // Dynamic Window / Caterpillar
+        return dynamicWindow(nums,k);
+        //return hashMapApproach(nums,k);
+    }
+
+    // PENDING 
+    public int hashMapApproach(int[] nums, int k) {
+        HashMap<Long,Integer> hm = new HashMap<>();
+        hm.put(1L,1);
+        long prod = 1;
+        int cnt=0;
+        int len = nums.length;
+        for(int i=0;i<len;i++){
+            prod *=nums[i];
+
+        }
+        return cnt;
+    }
+
+    // Friday, February 7, 2025 11:25:11 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int dynamicWindow(int[] nums, int k) {
+        // Dynamic Window
         int windowProd=1;
         int startIdx=0;
         int cnt = 0;
@@ -8,12 +29,13 @@ class Solution {
         for(int i=0;i<len;i++){
             windowProd *=nums[i];
             // Shrink the Window if product exceed k 
-            while(windowProd>=k && startIdx<=i){ 
+            while(windowProd>=k && startIdx<=i){  //<-- Out of Bound
                 windowProd /= nums[startIdx];
                 startIdx++;
             }
-            cnt+=(i-startIdx+1);           
+            cnt+=(i-startIdx+1);  // Why are we counting like this ?          
         }
         return cnt;
     }
+
 }
