dc1864b9a76df0b6b7c8a0172cee28e882d6fc6b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-07 19:58:58 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.15%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,16 +13,15 @@
  *     }
  * }
  */
+
+// Friday, February 7, 2025 7:58:18 PM
+// Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return false;
-        }
-        if(root.left==null && root.right==null && root.val == targetSum){
-            return true;
-        }
+        if(root==null) return false;
+        if(root.left==null && root.right==null && root.val == targetSum) return true;
         boolean leftSum = hasPathSum(root.left,targetSum-root.val);
         boolean rightSum = hasPathSum(root.right,targetSum-root.val);
-        return leftSum||rightSum;
+        return leftSum|rightSum; //<- BITWISE OR 
     }
 }
