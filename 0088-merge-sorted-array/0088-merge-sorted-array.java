dc7332606b988afa2c58f1b26852bc17d6a16796
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-08 15:06:37 +0530

    Time: 1 ms (30.02%), Space: 42.5 MB (26.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        bruteForce(nums1,m,nums2,n);
+        bruteForce(nums1,m,nums2,n); //<--
     }
 
     // Add all nums2 element to  nums1, Then Sort it
-    // TC n, (m+n)log(m+n)
+    // Saturday, February 8, 2025 3:05:38 PM
+    // Time Complexity:O(m+nlog(m+n)) | Space Complexity:O(1)
     public void bruteForce(int[] nums1, int m, int[] nums2, int n) {
+        // Copy nums2 elements at the end of nums1
         for(int i=0;i<n;i++){//<--
             nums1[i+m]=nums2[i];
         }
