d34f1be59e0022beea4c03d254aace6359fc6063
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-08 20:06:49 +0530

    Time: 0 ms (100%), Space: 40.6 MB (38.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,17 +4,18 @@ class Solution {
         return dpBottomUp(n);
     }
 
+    // Tuesday, January 7, 2025 7:12:32 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
     public int dpBottomUp(int n) {
         if(n<0) return 0;
         int[] dp = new int[n+1];
-        dp[0]=1; // Tricky Point
+        dp[0]=1; //<--
         dp[1]=1;
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];            
         }
         return dp[n];
     }    
-
 
     // Tuesday, January 7, 2025 7:06:13 PM
     // Time Complexity:O(2^n) | Space Complexity:O(1)
