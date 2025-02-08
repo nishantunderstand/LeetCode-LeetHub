a40172bc0c2edd2aa4779068c4c558dc7bb9f175
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-08 18:01:58 +0530

    Time: 32 ms (34.71%), Space: 45.2 MB (81.24%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        return linkedhashMapApproach(s);
+    }
+    // Saturday, February 8, 2025 6:01:15 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public int linkedhashMapApproach(String s){
+        LinkedHashMap<Character,Integer> hm = new LinkedHashMap<>();
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            hm.put(curr,hm.getOrDefault(curr,0)+1);
+        }
+
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            if(hm.get(curr)==1) return i;
+        }
+        
+        return -1;
+    }
+}
