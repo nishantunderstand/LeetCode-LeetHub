e274f7343279a147f582f134e995657e323f2f8e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-09 14:06:44 +0530

    Time: 20 ms (23.44%), Space: 55.6 MB (78.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,43 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {        
-        return hashSetApproach(nums);
+    public boolean containsDuplicate(int[] nums) {
+        //return bruteForceApproach(nums);        
+        return sortingApproach(nums);
+        //return hashSetApproach(nums);
+        
     }
 
     // Sunday, February 9, 2025 2:01:58 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
     public boolean hashSetApproach(int[] nums) {
         HashSet<Integer> number = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(number.contains(nums[i])){
+        for (int i = 0; i < nums.length; i++) {
+            if (number.contains(nums[i])) {
                 return true;
             }
             number.add(nums[i]);
         }
         return false;
     }
+
+    public boolean sortingApproach(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // TLE
+    public boolean bruteForceApproach(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
 }
