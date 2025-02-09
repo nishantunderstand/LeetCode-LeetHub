61bd78d33bfbab6105948c224c572c7420a82df8
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-09 14:02:48 +0530

    Time: 10 ms (88.4%), Space: 57.7 MB (66.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,11 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate(int[] nums) {        
+        return hashSetApproach(nums);
+    }
+
+    // Sunday, February 9, 2025 2:01:58 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public boolean hashSetApproach(int[] nums) {
         HashSet<Integer> number = new HashSet<>();
         for(int i=0;i<nums.length;i++){
             if(number.contains(nums[i])){
