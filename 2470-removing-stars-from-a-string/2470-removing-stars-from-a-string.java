30b5db0cc764daa2a3f17dfc0a54bf37db488b4b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-09 20:44:36 +0530

    Time: 86 ms (34%), Space: 46.3 MB (50.54%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -2,12 +2,35 @@
 
 class Solution {
     public String removeStars(String s) {
+        return forwardApproach(s);
+        // return backwardApproach(s);
+    }
+
+    // WILL LEAD TO INCORRECT RESULT 
+    public String backwardApproach(String s) {
+        Stack<Character> st = new Stack<>();
+        int len = s.length();
+        if(s==null || len==0) return "";
+        
+        for(int i=len-1;i>=0;i--){
+            char curr = s.charAt(i);
+            if(curr!='*') st.push(curr);  //<--     
+            else if(!st.isEmpty()) st.pop();
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+
+    public String forwardApproach(String s) {
         Stack<Character> st = new Stack<>();
         int len = s.length();
         if(s==null || len==0) return "";
         for(int i=0;i<len;i++){
             char curr = s.charAt(i);
-            if(curr!='*') st.push(curr);        
+            if(curr!='*') st.push(curr);  //<--     
             else if(!st.isEmpty()) st.pop();
         }
         StringBuilder sb = new StringBuilder();
