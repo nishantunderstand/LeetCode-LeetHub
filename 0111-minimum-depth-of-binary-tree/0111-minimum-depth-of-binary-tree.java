e3d714b2cd82c6942bb2a64b3e90ae1ee8fe1af0
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-09 19:14:32 +0530

    Time: 6 ms (21.8%), Space: 63.3 MB (57.81%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,17 +15,34 @@
  */
 // LeetCode 111
 // How different From 104. Maximum Depth of Binary Tree
+// Difference from 104 (Maximum Depth of Binary Tree): 
+// - 104 finds the longest path, using Math.max(left, right) + 1.
+// - 111 finds the shortest path to a leaf, so we need to handle cases where one child is missing. //<--
 class Solution {
     public int minDepth(TreeNode root) {
-        return dfs(root);
+        //return dfs(root);
+        return dfsReadable(root);
     }
 
+    public int dfsReadable(TreeNode root) {
+        if(root==null) return 0;
+        int left = dfs(root.left);
+        int right =dfs(root.right);
+        if(root.left==null) return right+1;
+        if(root.right==null) return left+1;
+        return Math.min(left,right)+1; //?? DON"T GET IT
+    }
+
+
+    // More Inutitive 
+    // Sunday, February 9, 2025 7:13:34 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
     public int dfs(TreeNode root) {
         if(root==null) return 0;
 
         int left = 1 + dfs(root.left);
         int right = 1+ dfs(root.right);
-        
+
         if(root.left==null) return right;
         if(root.right==null) return left;
 
