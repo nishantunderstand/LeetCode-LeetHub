508b89de347de649c3288840d75d569a04578f0a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-10 14:23:46 +0530

    Time: 3 ms (72.53%), Space: 45.1 MB (20.68%) - LeetHub

--- 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.java
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.java
@@ -20,19 +20,52 @@ class Node {
 
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
+        // return levelOrderForEach(root);
+        return levelOrderfor(root);
+    }
+
+    public List<List<Integer>> levelOrderfor(Node root) {
         Queue<Node> que = new LinkedList<>();
-        List<List<Integer>> res = new ArrayList<>();        
-        if(root==null) return res;        
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
         que.offer(root);
 
-        while(!que.isEmpty()){            
+        while (!que.isEmpty()) {
             int levelLen = que.size();
             List<Integer> curr = new ArrayList<>();
-            for(int i=0;i<levelLen;i++){
+            for (int i = 0; i < levelLen; i++) {
                 Node nodeElem = que.poll();
-                curr.add(nodeElem.val);                                  
-                for(Node child : nodeElem.children){
-                    if(child!=null){
+                curr.add(nodeElem.val);
+                for(int j=0;j<nodeElem.children.size();j++){
+                    if(nodeElem.children.get(j)!=null){
+                        que.offer(nodeElem.children.get(j));
+                    }
+                }
+            }
+            res.add(curr);
+        }
+        return res;
+    }
+
+
+    // Monday, February 10, 2025 2:20:19 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
+    public List<List<Integer>> levelOrderForEach(Node root) {
+        Queue<Node> que = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        que.offer(root);
+
+        while (!que.isEmpty()) {
+            int levelLen = que.size();
+            List<Integer> curr = new ArrayList<>();
+            for (int i = 0; i < levelLen; i++) {
+                Node nodeElem = que.poll();
+                curr.add(nodeElem.val);
+                for (Node child : nodeElem.children) {
+                    if (child != null) {
                         que.offer(child);
                     }
                 }
