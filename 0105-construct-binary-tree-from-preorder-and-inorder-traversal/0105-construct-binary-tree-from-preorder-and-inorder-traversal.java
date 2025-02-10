cf666db7b47c164ebbf2ea9b0b91301951681264
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-10 16:16:13 +0530

    Time: 3 ms (38.62%), Space: 44 MB (95.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,19 +15,17 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return constructTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+        return constructTreeRecursion(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
 
-    public TreeNode constructTree(int[] pre,int preBeg,int preEnd,int[] in,int inBeg,int inEnd){
-        if(preBeg>preEnd || inBeg>inEnd) return null;
-        
+    public TreeNode constructTreeRecursion(int[] pre,int preBeg,int preEnd,int[] in,int inBeg,int inEnd){
+        if(preBeg>preEnd || inBeg>inEnd) return null;        
         TreeNode root = new TreeNode(pre[preBeg]);
-        
         //int rootidx = findIdx(in,inBeg,inEnd,root.val);
         int rootidx = findIdx1(in,root.val);
         int leftSize = rootidx-inBeg; //<--
-        root.left = constructTree(pre,preBeg+1,preBeg+leftSize,in,inBeg,rootidx-1);
-        root.right = constructTree(pre,preBeg+leftSize+1,preEnd,in,rootidx+1,inEnd);
+        root.left = constructTreeRecursion(pre,preBeg+1,preBeg+leftSize,in,inBeg,rootidx-1);
+        root.right = constructTreeRecursion(pre,preBeg+leftSize+1,preEnd,in,rootidx+1,inEnd);
         return root;
     }
     public int findIdx(int[] arr,int start,int end,int target){
