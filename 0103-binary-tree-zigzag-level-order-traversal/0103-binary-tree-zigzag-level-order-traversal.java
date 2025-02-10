b95ae92322b17422cdc20ecd8c5318024da87dd2
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-10 13:57:48 +0530

    Time: 1 ms (69.76%), Space: 42.6 MB (14%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,6 +15,36 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //return zigzagLevelOrderTrueFalse(root);
+        return zigzagLevelOrderToggle(root);
+    }
+    public List<List<Integer>> zigzagLevelOrderTrueFalse(TreeNode root) {
+        Queue<TreeNode> que = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();        
+        if(root==null) return res;        
+        que.offer(root);
+        boolean LR = true; //<--
+        while(!que.isEmpty()){            
+            int levelLen = que.size();
+            List<Integer> curr = new ArrayList<>();
+            for(int i=0;i<levelLen;i++){
+                TreeNode node = que.poll();
+                if(LR){
+                    curr.addLast(node.val);                    
+                }else{
+                    curr.addFirst(node.val);
+                }                
+                if(node.left!=null) que.offer(node.left);
+                if(node.right!=null) que.offer(node.right);
+            }
+            res.add(curr);
+            LR=!LR; // Toggling IT
+            //LR=false; // Setting it False Permantelly
+        }
+        return res;
+    }
+
+    public List<List<Integer>> zigzagLevelOrderToggle(TreeNode root) {
         Queue<TreeNode> que = new LinkedList<>();
         List<List<Integer>> res = new ArrayList<>();        
         if(root==null) return res;        
