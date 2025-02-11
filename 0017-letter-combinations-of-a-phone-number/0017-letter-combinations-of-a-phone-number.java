c8c397a646945f9e3cbade18ad989f2247e99e19
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-11 16:32:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (65.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,8 @@
 // LeetCode 17
 
 class Solution {
+    /**
+    // If you plan to declare here that's how you will declare
     private static final HashMap<Character,String> hm = new HashMap<>();
     static{
         hm.put('0',"0");
@@ -16,14 +18,29 @@ class Solution {
         
     }
 
+    */
+
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         if(digits==null || digits.isEmpty()) return res;
-        backtracking(res,new StringBuilder(),digits,0);
+
+        HashMap<Character,String> hm = new HashMap<>();
+        hm.put('0',"0");
+        hm.put('1',"1");
+        hm.put('2',"abc");
+        hm.put('3',"def");
+        hm.put('4',"ghi");
+        hm.put('5',"jkl");
+        hm.put('6',"mno");
+        hm.put('7',"pqrs");
+        hm.put('8',"tuv");
+        hm.put('9',"wxyz");
+
+        backtracking(res,new StringBuilder(),digits,0,hm);
         return res;
     }
     
-    public void backtracking(List<String> res,StringBuilder curr,String digits,int index){
+    public void backtracking(List<String> res,StringBuilder curr,String digits,int index,HashMap<Character,String> hm){
         if(index == digits.length()){
             res.add(curr.toString());
             return;
@@ -34,7 +51,7 @@ class Solution {
 
         for(char ch : letters.toCharArray()){
             curr.append(ch);
-            backtracking(res,curr,digits,index+1);
+            backtracking(res,curr,digits,index+1,hm);
             curr.deleteCharAt(curr.length()-1); //<--
         }                
     }
