321fa3e456e57ebcf849ceb1f78130c385b3afd8
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-14 20:17:09 +0530

    Time: 5 ms (36.77%), Space: 42.2 MB (30.75%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -2,8 +2,13 @@
 
 class Solution {
     public int maximumDifference(int[] nums) {
+        return bruteForce(nums);
+    }
+
+    // Friday, February 14, 2025 8:15:53 PM
+    // Time Complexity:O(n^2) | Space Complexity:O(1)
+    public int bruteForce(int[] nums) {
         int len = nums.length;
-        // int max = Integer.MIN_VALUE; // 
         int max = -1; 
 
         for(int i=0;i<len;i++){
@@ -16,4 +21,18 @@ class Solution {
         }
         return max;
     }
+    
+    // FAILED
+    public int bruteForceFAILED(int[] nums) {
+        int len = nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                int csum = nums[j]-nums[i];
+                max = Math.max(max,csum);
+            }
+        }
+        return max==Integer.MIN_VALUE?-1:max;
+    }
+
 }
