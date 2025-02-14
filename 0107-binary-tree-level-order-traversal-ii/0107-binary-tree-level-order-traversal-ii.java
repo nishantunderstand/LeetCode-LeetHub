e4397f1e7aaf3fb616028527ca9e98931523f0aa
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-14 14:08:01 +0530

    Time: 1 ms (94.47%), Space: 42.8 MB (84.84%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -15,9 +15,34 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return levelOrderBottomApproach(root);
+        //return levelOrderBottomApproach(root);
         //return levelOrderBottomApproachModified(root);
+        return levelOrderBottomApproachEducativeIO(root);
     }
+
+    public List<List<Integer>> levelOrderBottomApproachEducativeIO(TreeNode root) {
+        
+        List<List<Integer>> res = new LinkedList<>(); //<--
+        
+        if (root == null) return res;
+        Queue<TreeNode> que = new LinkedList<>();
+
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int levelSize = que.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize); //<--
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = que.poll();
+                currentLevel.add(currentNode.val);
+
+                if (currentNode.left != null) que.offer(currentNode.left);
+                if (currentNode.right != null) que.offer(currentNode.right);
+            }
+            res.add(0,currentLevel);
+        }
+        return res;
+    }
+
 
     // Monday, February 10, 2025 1:37:30 PM
     // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
