3242e457847c3305eb429cc71c5f6d09e47b3713
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-15 16:07:30 +0530

    Time: 1 ms (99.75%), Space: 48 MB (22.99%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -8,23 +8,27 @@ class Solution {
             Arrays.fill(rowArr,-1);
         }
         return dpTopDown(grid,grid.length-1,grid[0].length-1,dp);
+        //return dpBottomUp(grid);
     }
 
+    public int dpBottomUp(int[][] grid,int i,int j,int[][] dp){
+        return 0;
+    }
+
+    // Monday, January 13, 2025 7:40:35 PM
+    // Time Complexity:O(m*n) | Space Complexity:O(m*n)
     public int dpTopDown(int[][] grid,int i,int j,int[][] dp){
-        if(i==0 && j==0) return grid[0][0];
+        if(i==0 && j==0) return grid[0][0]; //<--
         if(i<0 || j <0) return Integer.MAX_VALUE;
         if(dp[i][j]!=-1) return dp[i][j];
 
         int up =  dpTopDown(grid,i-1,j,dp);
         int left = dpTopDown(grid,i,j-1,dp);
         dp[i][j] = grid[i][j] + Math.min(up,left);
-        return dp[i][j];
+        return dp[i][j]; // Whyn't dp[0][0] ??
     }
 
-
-
-    // If you want to find Minimium , Then return a very large value.
-
+    // If you want to find Minimium , Then you have to return a very large value.
     // TLE 
     // Monday, January 13, 2025 7:19:45 PM
     // Time Complexity:O(2^n) | Space Complexity:O(n)
