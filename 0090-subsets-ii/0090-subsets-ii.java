df1fb4da3aceb94683c0648c8c5c2cdf55218119
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-15 21:53:23 +0530

    Time: 1 ms (99.75%), Space: 43.9 MB (12.22%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,20 @@
+// Leetcode : 90
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums); //<--
         int len = nums.length;
         backtracking(0,nums,res,curr);
         return res;
     }
-
-
+    
+    // Saturday, February 15, 2025 9:52:34 PM
+    // Time Complexity:O(2^n) | Space Complexity:O(n)
     public void backtracking(int idx,int[] nums,List<List<Integer>> res,List<Integer> curr){
         res.add(new ArrayList<>(curr));    
         for(int i=idx;i<nums.length;i++){
-            if(i>idx && nums[i]==nums[i-1]) continue;
+            if(i>idx && nums[i]==nums[i-1]) continue; //<--
 
             curr.add(nums[i]);            
             backtracking(i+1,nums,res,curr);
