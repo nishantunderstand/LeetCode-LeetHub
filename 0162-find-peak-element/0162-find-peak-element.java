54084ce36681065b0eb15192955559b8636e6921
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-16 16:39:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (93.89%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,3 +1,4 @@
+// Leetcode : 162
 /**
  * Postive Array
  * Negative Array
@@ -21,10 +22,30 @@
 
 class Solution {
     public int findPeakElement(int[] nums) {
-        return bruteForce(nums);
+        //return binarySearch(nums);
+        return bruteForceSearch(nums);
     }
 
-    public int bruteForce(int[] nums) {
+
+    public int bruteForceSearch(int[] nums) {
+        int n = nums.length;
+        int len = n;
+        if (nums == null || n == 0) {
+            return 0;
+        }
+        if(len==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        
+        for(int i=1;i<len-1;i++){
+            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return -1;        
+    }
+
+    public int binarySearch(int[] nums) {
         int n = nums.length;
         if (nums == null || n == 0) {
             return 0;
