697bbaf24fead57737ea2e02f2a19cbe46eb9a89
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-16 20:22:19 +0530

    Time: 0 ms (100%), Space: 41 MB (82.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,9 +16,14 @@
 
 // Monday, December 30, 2024 11:35:51 PM
 // Time Complexity:O(n) | Space Complexity:OHeightOfTree)
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {        
-        // if(p==null || q==null) return false; // <-- THIS WILL GENERATE ERROR ?? WHY ??? 
+        // if(p==null || q==null) return false; // <-- THIS WILL GENERATE ERROR 
+        /**
+        It does not check if both p and q are null.
+        If both are null, the correct answer should be true, but this line incorrectly returns false.
+        */
         if(p==null && q==null) return true;
         if(p==null || q==null) return false; 
         if(p.val!=q.val) return false;
