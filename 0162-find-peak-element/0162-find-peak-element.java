cd6c46e01a2338d9940cc35484d234af365b1301
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-16 17:48:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (13.11%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,4 +1,4 @@
-// Leetcode : 162
+
 /**
  * Postive Array
  * Negative Array
@@ -19,29 +19,40 @@
  * I don't need to think in the direction of lo and hi, Instead i need to think
  * in something else direction.
  */
-
+// Leetcode : 162
 class Solution {
     public int findPeakElement(int[] nums) {
         //return binarySearch(nums);
-        return bruteForceSearch(nums);
+        //return bruteForceSearch(nums);
+        return bruteForceSearchPeak(nums);
     }
-    // Sunday, February 16, 2025 4:39:55 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
-    public int bruteForceSearch(int[] nums) {
+    // [2,1] Failed for this
+    // [1,2,3]
+    public int bruteForceSearchPeak(int[] nums) {
         int n = nums.length;
-        int len = n;
-        if (nums == null || n == 0) return 0;
+        if (nums == null || n == 0) {
+            return 0;
+        }
 
-        if(len==1) return 0;
+        if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
         
-        for(int i=1;i<len-1;i++){
-            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
-                return i;
+        int lo = 1;
+        int hi = n-2;
+        
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(nums[mid] > nums[mid-1]){
+                lo = mid+1;
+            }else{
+                hi=mid; //<--
             }
         }
-        return -1;        
+        return lo; //<--
     }
 
     // Sunday, February 16, 2025 4:42:32 PM
@@ -62,5 +73,25 @@ class Solution {
             }
         }
         return lo;
+    }
+
+
+    // Sunday, February 16, 2025 4:39:55 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int bruteForceSearch(int[] nums) {
+        int n = nums.length;
+        int len = n;
+        if (nums == null || n == 0) return 0;
+
+        if(len==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        
+        for(int i=1;i<len-1;i++){
+            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return -1;        
     }
 }
