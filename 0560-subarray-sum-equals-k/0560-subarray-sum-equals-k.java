ac78ffb9ca6e5202d007a1dd99c1b6d5a73908bb
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-19 14:14:48 +0530

    Time: 26 ms (37.28%), Space: 46.5 MB (63.96%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,14 +1,38 @@
+// Leetcode : 560
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // return bruteForceApproach(nums,k);
-        return hashMapApproach(nums,k);
-        // return slidingWindowCaterpillarMethod(nums, k);
+        //return bruteForceApproach(nums,k);
+        //return slidingWindowCaterpillarMethodPOSTIVEARRAYONLY(nums, k); // FAILS For K==0
+        return prefixhashMapApproach(nums,k);
+       
     }
+
+
+    // T.C - O(n)| S.C - O(n)
+    public int prefixhashMapApproach(int[] nums, int k) {
+        int n = nums.length;
+        if (n <= 0)
+            return 0;
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+        int csum = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            csum += nums[i];
+            if (hm.containsKey(csum - k)) {
+                cnt += hm.get(csum - k);
+            }
+            hm.put(csum, hm.getOrDefault(csum, 0) + 1);
+        }
+        return cnt;
+    }
+
 
     // Sunday, June 9, 2024 2:17:34 PM
     // Time Complexity: O(n) | Space Complexity: O(1)
-    // Cannot handle k==0
-    public int slidingWindowCaterpillarMethod(int[] nums, int k) {
+    // Cannot handle [1] k=0 , CODE FAILS
+    public int slidingWindowCaterpillarMethodPOSTIVEARRAYONLY(int[] nums, int k) {
         int count = 0;
         int windowSum = 0;
         int start = 0;
@@ -29,6 +53,10 @@ class Solution {
         return count;
     }
 
+    
+
+    
+
     // Wednesday, December 11, 2024 9:46:20 PM
     // T.C - O(n^2) | S.C - O(1)
     public int bruteForceApproach(int[] nums, int k) {
@@ -41,26 +69,6 @@ class Solution {
                     cnt++;
                 }
             }
-        }
-        return cnt;
-    }
-
-    // T.C - O(n)| S.C - O(n)
-    public int hashMapApproach(int[] nums, int k) {
-        int n = nums.length;
-        if (n <= 0)
-            return 0;
-
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        hm.put(0, 1);
-        int csum = 0;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            csum += nums[i];
-            if (hm.containsKey(csum - k)) {
-                cnt += hm.get(csum - k);
-            }
-            hm.put(csum, hm.getOrDefault(csum, 0) + 1);
         }
         return cnt;
     }
