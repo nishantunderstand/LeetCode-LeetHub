e01e884f2759c8563a6dfadba9b9f856dfa93ca6
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-21 19:14:33 +0530

    Time: 0 ms (100%), Space: 40.4 MB (49.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
         //return recursive(n);
-        return dpBottomUp(n);
+        //return dpBottomUp(n);
+        return dpMemo(n,dp);
+    }
+
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public int dpMemo(int n,int[] dp) {
+        if(n<0) return 0;
+        if(n==1||n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = dpMemo(n-1,dp) + dpMemo(n-2,dp);
+        return dp[n];
     }
 
     // Tuesday, January 7, 2025 7:12:32 PM
