bd7b6a44a76bc2ac1ecbd0eb5217daa50dde02d3
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-21 19:16:29 +0530

    Time: 33 ms (77.13%), Space: 57.4 MB (81.18%) - LeetHub

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -0,0 +1,36 @@
+// LeetCode 2461
+
+// Friday, February 21, 2025 7:15:32 PM
+// Time Complexity:O(n) | Space Complexity:O(k)
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int len = nums.length;
+        long windowSum = 0;
+        long maxSum = 0;
+        int windowStart = 0;
+        HashSet<Integer> hs = new HashSet<>(); //<--
+        for(int windowEnd=0;windowEnd<len;windowEnd++){
+            // If duplicate is found, shrink the window
+            while(hs.contains(nums[windowEnd])){
+                hs.remove(nums[windowStart]);
+                windowSum -= nums[windowStart];
+                windowStart++;
+            }
+
+            // Add the new element to the set and sum            
+            hs.add(nums[windowEnd]);
+            windowSum+=nums[windowEnd];
+            
+            // Check if we have a valid window of size `k`
+            if(windowEnd-windowStart+1==k){ //<-- b - a + 1
+                maxSum = Math.max(maxSum,windowSum);
+
+                // Move the window forward
+                hs.remove(nums[windowStart]);                
+                windowSum -= nums[windowStart];                
+                windowStart++;
+            }
+        }
+        return maxSum;
+    }
+}
