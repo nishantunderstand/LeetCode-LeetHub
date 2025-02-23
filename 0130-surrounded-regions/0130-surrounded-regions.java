e2eff1ea04a66c2b2d961c2b221b501523f5d6be
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-23 17:31:52 +0530

    Time: 2 ms (86.16%), Space: 46.1 MB (12.14%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -11,22 +11,31 @@ class Solution {
     public void striverSurroundedApproach(char[][] board) {
         int rows = board.length;
         int cols = board[0].length;
-        boolean[][] vis = new boolean[rows][cols];
-        
+        boolean[][] vis = new boolean[rows][cols];        
         if(rows==0) return;
 
+        /**
         // First Column and Last => Column ROW INDEX
-        for(int i=0;i<rows;i++){
-            
+        for(int i=0;i<rows;i++){            
             if(!vis[i][0] && board[i][0]=='O') dfs(board,vis,i,0); // First Col
             if(!vis[i][cols-1] && board[i][cols-1]=='O') dfs(board,vis,i,cols-1); // Last Col            
         }        
-
         // First and Last Row => COLUMN INDEX
         for(int j=0;j<cols;j++){
             if(!vis[0][j] && board[0][j]=='O') dfs(board,vis,0,j); // Firs Row
             if(!vis[rows-1][j] && board[rows-1][j]=='O') dfs(board,vis,rows-1,j);   // Last Row                      
         }
+        */
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if( (i==0 || i==rows-1 || j==0 || j==cols-1) && board[i][j]=='O'){
+                    dfs(board,vis,i,j);
+                }
+            }
+        }
+
+
+
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
