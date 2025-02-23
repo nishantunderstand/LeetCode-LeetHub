076ffb5d2d376a46e290f58847832a7840b33518
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-23 19:27:34 +0530

    Time: 7 ms (87.27%), Space: 56.5 MB (55.89%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -0,0 +1,40 @@
+// Leetcode : 1020
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        // This is Modified Version Of Surrounded Region
+        // Mark Cell that are connected to first row/Col or last row/col to Zero
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if((i==0 || i==rows-1 || j==0 || j==cols-1) && grid[i][j]==1){
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;        
+    }
+    public void dfs(int[][] grid,int i,int j){
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(i<0 || i>=rows || j<0 || j>=cols) return;
+        if(grid[i][j]!=1) return;
+        
+        grid[i][j]=0; //<--
+        
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
