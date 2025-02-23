1953a54f54a9c1e9769325078d6ada5e0c83143a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-23 22:30:32 +0530

    Time: 2 ms (79.89%), Space: 46.2 MB (8.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,6 +2,23 @@
 
 class Solution {
     public boolean canJump(int[] nums) {
+        //return striverApproach(nums);
+        return striverApproachMOD(nums);
+    }
+
+    public boolean striverApproachMOD(int[] nums) {
+        int maxReach =  0;
+        int len = nums.length;
+        for(int i=0;i<len;i++){
+            if(i>maxReach) return false; // How did you reach ??
+            maxReach = Math.max(maxReach,i+nums[i]);
+            if(maxReach>=len-1) return true; // Optimization
+        }
+        return true; // Reached the end
+    }
+
+
+    public boolean striverApproach(int[] nums) {
         int maxReach =  0;
         int len = nums.length;
         for(int i=0;i<len;i++){
