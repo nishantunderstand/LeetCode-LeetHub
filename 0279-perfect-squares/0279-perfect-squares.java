b2c1c7f6da5c4688ff17d67a5ba6d09b5ec8a412
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-24 19:19:19 +0530

    Time: 504 ms (7.18%), Space: 52.9 MB (9.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,10 +1,28 @@
+// Leetcode : 279
 class Solution {
     public int numSquares(int n) {
         //return recursive(n);
         int [] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return dpTopDown(n,dp);
+        HashMap<Integer,Integer> memo = new HashMap<>();
+        //return dpTopDown(n,dp);
+        return dpHashMemo(n,memo);
     }
+
+    // Monday, February 24, 2025 7:18:58 PM
+    // Time Complexity:O(n*sqrt(n)) | Space Complexity:O(n)
+    public int dpHashMemo(int n,HashMap<Integer,Integer> memo){
+        if(n<=3) return n; 
+        
+        if(memo.containsKey(n)) return memo.get(n);
+        int ways = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            ways = Math.min(ways,1 + dpHashMemo(n-(i*i),memo));
+        }
+        memo.put(n,ways);
+        return ways;
+    }
+
 
     // Tuesday, January 7, 2025 7:44:46 PM
     // Time Complexity:O(n*sqrt(n)) | Space Complexity:O(n)
