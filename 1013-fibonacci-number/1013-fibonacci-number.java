fba319115c5a23da31e83db713a1026744bd64a8
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-02-24 19:11:32 +0530

    Time: 0 ms (100%), Space: 40.4 MB (47.97%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,20 @@
+// Leetcode : 509
 class Solution {
     public int fib(int n) {
-        return recursiveSol(n);
+        HashMap<Integer,Integer> memo = new HashMap<>();
+        //return recursiveSol(n);
+        return dpHashMemo(n,memo);
     }
+
+    public int dpHashMemo(int n,HashMap<Integer,Integer> memo){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(memo.containsKey(n)) return memo.get(n);
+        int ways = dpHashMemo(n-1,memo) + dpHashMemo(n-2,memo);
+        memo.put(n,ways);
+        return ways;
+    }
+
 
     public int recursiveSol(int n) {
         if(n==0){
