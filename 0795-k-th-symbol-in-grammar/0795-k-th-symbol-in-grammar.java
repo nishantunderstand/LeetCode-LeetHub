bbbd8eb904ed35d416eadeb102eaea6e6754cba6
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-13 21:50:21 +0530

    Time: 0 ms (100%), Space: 40.2 MB (81.13%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
@@ -4,11 +4,16 @@ class Solution {
         return findKthSymbolMIK(n,k);
     }
     // Thursday, March 13, 2025 9:47:02 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
+    // Time Complexity:O(n) | Space Complexity:O(n)
     public int findKthSymbolMIK(int n,int k){
         if(n==1) return 0; //<-- 
 
-        int len = (int) Math.pow(2,n-1);
+        // Approach 1
+        int len1 = (int) Math.pow(2,n-1);
+        
+        // Approach 2
+        int len = 1<<(n-1); //<-- We Need ONLY n-2
+
         int mid = len/2;
 
         if(k<=mid){ //<--
