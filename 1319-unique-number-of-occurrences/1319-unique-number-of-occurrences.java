12663faf4a130335381fdb819a1a8db7651bb92e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-16 21:45:23 +0530

    Time: 2 ms (98.09%), Space: 42.5 MB (5.64%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,15 +1,27 @@
 /** 
-Create HashMap
-Key , Value 
+Create HashMap => Key , Value 
 If the Value of hashmap are unique, then return true else return false
+
+[1,2,2,1,1,3]
+1 - 3
+2 - 2
+3 - 1
+Everything is Distinct | Return True
+
+
+[1,2]
+1 - 1
+2 - 1
+Same | Return False
+
 */
+
 
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         return hashMapAndHashSet(arr);
 
     }
-
     // Friday, February 7, 2025 5:54:15 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
     public boolean hashMapAndHashSet(int[] arr) {
@@ -17,7 +29,7 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }        
-        HashSet<Integer> hs = new HashSet<>(hm.values()); 
+        HashSet<Integer> hs = new HashSet<>(hm.values()); //<--
         return hs.size()== hm.size(); //<---
     }
 }
