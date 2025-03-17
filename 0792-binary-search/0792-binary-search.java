2516482d299983e56abbafcd973abb65e4b98070
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-17 09:16:16 +0530

    Time: 0 ms (100%), Space: 45.9 MB (45.19%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,8 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {        
-        return iterativeBinarySearch(nums,target);
+        //return iterativeBinarySearch(nums,target);
+        return defaultMethod(nums,target);
     }
 
+    // Monday, March 17, 2025 9:15:02 AM
+    // Time Complexity:O(logn) | Space Complexity:O(1)
     public int iterativeBinarySearch(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
@@ -19,5 +22,13 @@ class Solution {
 
         }
         return -1;
+    }
+
+    // Monday, March 17, 2025 9:15:02 AM
+    // Time Complexity:O(logn) | Space Complexity:O(1)
+    public int defaultMethod(int[] nums, int target) {
+        int i = Arrays.binarySearch(nums, target);
+        return i < 0 ? -1 : i;
+
     }
 }
