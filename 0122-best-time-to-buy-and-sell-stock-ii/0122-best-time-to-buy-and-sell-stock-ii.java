8986935ad9bc0dc0417614028510a4a4f06e2b09
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-18 19:59:29 +0530

    Time: 1 ms (76.79%), Space: 45.5 MB (93.08%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 // Leetcode : 122
 // Leetcode : 121 How Different From it ?
+
 class Solution {
     public int maxProfit(int[] prices) {        
         return greedyApproach(prices);
     }
-
-
+    
     // Multiple Buy and Sell Possible
     // Sunday, January 26, 2025 9:44:24 PM
     // Time Complexity:O(n) | Space Complexity:O(1)
@@ -15,7 +15,7 @@ class Solution {
         int profit = 0;
         for(int i=1;i<n;i++){
             if(prices[i-1]<prices[i]){
-                profit+=prices[i]-prices[i-1];
+                profit+=prices[i]-prices[i-1]; //<--
             }
         }
         return profit;
