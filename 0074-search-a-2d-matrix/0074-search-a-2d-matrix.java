1ad38086230ee2748c7671e2587568417f98f683
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-18 18:47:44 +0530

    Time: 0 ms (100%), Space: 42 MB (80.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        // return bruteForce(matrix, target);
-        // return optimizedByCornerSearch(matrix, target);
-        return binarySearchApproach(matrix, target);
+        //return normalSearch(matrix, target);
+        //return SearchByCornerSearch(matrix, target);
+        return flattenbinarySearchApproach(matrix, target);
     }
 
     // Wednesday, December 18, 2024 1:58:27 PM
-    // Time Complexity:O(log(m*n) | Space Complexity:O(1)
-    // Do i need to flatten a Matrix then Search ?
-    public boolean binarySearchApproach(int[][] matrix, int target) {
+    // Time Complexity:O(log(m*n)) | Space Complexity:O(1)
+    // Do i need to flatten a Matrix then Search ? NO
+    public boolean flattenbinarySearchApproach(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
         int low = 0;
@@ -16,11 +16,12 @@ class Solution {
         
         while(low<=high){
             int mid = low + (high-low)/2;
+            int midElement = matrix[mid/col][mid%col];
 
-            if(matrix[mid/col][mid%col]==target){
+            if(midElement==target){
                 return true;
             }
-            else if(matrix[mid/col][mid%col]<target){
+            else if(midElement<target){
                 low=mid+1;
             }else{
                 high=mid-1;
@@ -32,9 +33,9 @@ class Solution {
 
     // Wednesday, December 18, 2024 1:31:25 PM
     // Time Complexity:O(m+n) | Space Complexity:O(1)
-        // Top Right Corner (0,col-1)
-        // When you change Corner, You need to modify the codebase as well.
-    public boolean optimizedByCornerSearch(int[][] matrix, int target) {
+    // Top Right Corner (0,col-1)
+    // When you change Corner, You need to modify the codebase as well.
+    public boolean SearchByCornerSearch(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
         int i = 0;
@@ -53,8 +54,8 @@ class Solution {
     }
 
     // Wednesday, December 18, 2024 1:31:21 PM
-    // Time Complexity:O(n^2) | Space Complexity:O(1)
-    public boolean bruteForce(int[][] matrix, int target) {
+    // Time Complexity:O(m*n) | Space Complexity:O(1)
+    public boolean normalSearch(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == target) {
