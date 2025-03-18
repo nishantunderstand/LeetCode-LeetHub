c33557994cd376278e2262da2c1f14481678bf47
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-18 20:05:44 +0530

    Time: 2 ms (87.93%), Space: 56.4 MB (16.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,5 +1,50 @@
+// LeetCode 238
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] nums) {    
+        return prefixAndSuffixApproach(nums);
+        //return bruteForceApproach(nums);
+        //return byDivision(nums);
+    }
+
+    // [-1,1,0,-3,3]
+    // Failed If Zero is Present in the Array
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private int[] byDivision(int[] nums) {
+        int total = 1;
+        for(int num:nums){
+            total*=num;
+        }
+        int[] res = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                res[i]=total/nums[i]; //<--
+            }            
+        }
+        return res;
+    }
+
+    // TLE
+    // Time Complexity:O(n^2) | Space Complexity:O(1)
+    private int[] bruteForceApproach(int[] nums) {
+        // Create a result Array
+        int[] res = new int[nums.length];
+        // Calcuate | Except Itself
+        for(int i =0;i<nums.length;i++){
+            int prod=1;
+            for(int j=0;j<nums.length;j++){
+                if(i==j) continue;
+                prod*=nums[j];
+            }
+            res[i]=prod;
+        }
+
+        // Return
+        return res;
+    }
+
+    // Tuesday, March 18, 2025 8:04:59 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private int[] prefixAndSuffixApproach(int[] nums) {
         int[] prefix = new int[nums.length];
         int[] suffix = new int[nums.length];
         prefix[0]=1;
@@ -18,5 +63,5 @@ class Solution {
             ans[i]=prefix[i]*suffix[i];
         }
         return ans;
-    }
+    }    
 }
