a673475465be5b76ae5470efcdb1a614cbcff61f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-18 18:45:18 +0530

    Time: 1 ms (82.93%), Space: 43.1 MB (69.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -3,16 +3,15 @@ class Solution {
         return recursiveSol(n);
     }
 
-
-
-
     // Monday, November 18, 2024 7:02:48 PM
     // T.C - O(2^2n) | S.C - O(n)
     public List<String> recursiveSol(int n){
         List<String> res = new ArrayList<>();
         if(n<=0) return res;
-        // recursiveHelper(res,"",0,0,n);
-        recursiveHelper2(res,"",n,n);
+        
+        //recursiveHelper(res,"",0,0,n); //<--
+        
+        recursiveHelper2(res,"",n,n); //<--
         return res;
     }
     // Tuesday, November 19, 2024 11:34:32 AM
@@ -25,28 +24,30 @@ class Solution {
         if(open>0){
             recursiveHelper2(res,curr+"(",open-1,close);
         }
-        if(close>open){
+        if(close>open){ //<--
             recursiveHelper2(res,curr+")",open,close-1);
         }
     }
 
     public void recursiveHelper(List<String> res,String curr,int open,int close,int n){
         // if(open>close) return; This Will LEAD TO WRONG RESULT.we mistakenly prune out valid recursive
+
         /** Base Case V1
         if(curr.length()==2*n){
             res.add(curr);
             return;
         }
         */
+
         if(open==n && close==n){
             res.add(curr);
             return;
         }
         
-        if(open<n){
+        if(open<n){ //<--
             recursiveHelper(res,curr+"(",open+1,close,n);
         }
-        if(close<open){
+        if(close<open){ //<--
             recursiveHelper(res,curr+")",open,close+1,n);
         }
     }
