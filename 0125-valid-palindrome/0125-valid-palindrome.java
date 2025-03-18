b2660c6eaa60e43d1c68d1ee77944eb45825dbfc
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-18 20:01:55 +0530

    Time: 2 ms (99.07%), Space: 42.4 MB (99.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,10 +7,29 @@ Learn Regex
 Here we are comparing Character
 */
 
+
+// s consists only of printable ASCII characters.
+
 class Solution {
     public boolean isPalindrome(String s) {
         //return stringInBuiltMethodApproach(s);
-        return pointerApproach(s);
+        // return pointerApproach(s);
+        return characterInBuildMethod(s);
+    }
+
+    // Tuesday, March 18, 2025 8:01:08 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    private boolean characterInBuildMethod(String s){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)) ){
+                return false;
+            }
+            i++; j--;
+        }
+        return true;
     }
 
     // Tuesday, February 11, 2025 7:29:23 PM
@@ -58,3 +77,22 @@ class Solution {
         return true;
     }
 }
+
+/**
+
+ASCII
+
+- ASCII
+- American Standard Code for Information Interchange
+- Control Character
+    - Control Characters in ASCII (0-31 and 127)
+    - They are not printable
+- Non control character
+    - They are printable
+    - Space (32)
+    Symbols (33-47, 58-64, 91-96, 123-126): ! @ # $ % ^ & * ( ) - _ = + { } [ ] | ; : ' " , < . > / ? \
+    - Digits (48-57): 0-9
+    - Uppercase letters (65-90): A-Z
+    - Lowercase letters (97-122): a-z
+
+*/
