39c18374967c1fe1c3228b19d4023aed02ff3d86
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-20 20:38:41 +0530

    Time: 0 ms (100%), Space: 44.9 MB (53.59%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.java
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -1,11 +1,37 @@
 // Leetcode : 867
 class Solution {
     public int[][] transpose(int[][] matrix) {        
-        return newArray(matrix);
+        //return newArray(matrix);
+        return bySwap(matrix);
     }
+
+    // Thursday, March 20, 2025 8:32:27 PM
+    // T.C - O(row*col) | S.C - O(1)
+    // For Square Matrix Only
+    public int[][] bySwap(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        if(matrix==null || row==0||col==0) return new int[0][0];
+        
+        // Handle Rectangle Matrix
+        if(row!=col) return newArray(matrix);
+
+        // Upper Half
+        // InPlace Transpose For Square Matrix
+        for(int i=0;i<row;i++){
+            for(int j=i+1;j<col;j++){ //<--
+               int temp = matrix[i][j];
+               matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        return matrix;
+    }
+
 
     // Friday, November 15, 2024 8:56:58 PM
     // T.C - O(row*col) | S.C - O(row*col)
+    // For Square & Rectangle Matrix
     public int[][] newArray(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
