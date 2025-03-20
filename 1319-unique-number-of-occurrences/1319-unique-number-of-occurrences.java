4f4a5356f0fa5b38ba84757b3eb5ec36b4dad25d
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-20 20:11:30 +0530

    Time: 2 ms (98.2%), Space: 41.9 MB (54.43%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -16,21 +16,39 @@ Same | Return False
 
 */
 
-
+// Leetcode : 1207
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         return hashMapAndHashSet(arr);
+        //return hashMapAndHashSetMOD(arr);
 
     }
     // Friday, February 7, 2025 5:54:15 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
-    public boolean hashMapAndHashSet(int[] arr) {
+    public boolean hashMapAndHashSetMOD(int[] arr) {
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0;i<arr.length;i++){
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }        
         HashSet<Integer> hs = new HashSet<>(hm.values()); //<--
         return hs.size()== hm.size(); //<---
+    }
+
+    // [1,2,2,1,1,3]
+    // 1-3, 2-2, 3-1
+    public boolean hashMapAndHashSet(int[] arr) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        HashSet<Integer> hs = new HashSet();
+        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
+            if(hs.contains(entry.getValue())){
+                return false;
+            }
+            hs.add(entry.getValue());
+        }
+        return true;       
     }
 }
 
