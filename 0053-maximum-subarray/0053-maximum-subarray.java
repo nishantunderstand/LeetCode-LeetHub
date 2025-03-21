9bc9f06552fa8e7268971304eae793afbf49c77a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-21 18:31:53 +0530

    Time: 1 ms (99.54%), Space: 57.1 MB (42.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -2,10 +2,53 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         //return bruteForce(nums); 
-        return kadaneAlgorithm(nums);
+        //return kadaneAlgorithm(nums);
         //return prefixSum(nums); 
-        //return carryForward(nums);
+        return carryForward(nums);
+        //return slidingWindow(nums);
+    }
 
+    private int prefixSum(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n+1];
+        // Out of Place - Prefix Sum
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i] + nums[i];
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                maxSum = Math.max(maxSum, prefix[j+1]-prefix[i]);
+            }
+        }
+        return maxSum;
+    }
+
+
+    private int carryForward(int[] nums) {
+        int n = nums.length;    
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            maxSum = Math.max(maxSum,sum);
+            if(sum<0) sum=0; // Reset Sum
+        }
+        return maxSum;
+    }
+
+
+    private int slidingWindowFailedNegative(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int windowSum = 0;
+        int windowStart = 0;
+        for(int windowEnd=0;windowEnd<nums.length;windowEnd++){
+            windowSum += nums[windowEnd];
+            maxSum = Math.max(maxSum,windowSum);
+            // This is where the sliding window approach fails: 
+            // We don't handle negative contributions properly.
+        }
+        return maxSum;
     }
 
     // Tuesday, December 17, 2024 10:46:30 PM
