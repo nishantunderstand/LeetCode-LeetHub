d7f026c3b714c5a2902e408960f658a3576533ef
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-03-21 18:03:10 +0530

    Time: 0 ms (100%), Space: 41.3 MB (42.8%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -8,10 +8,14 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode middleNode(ListNode head) {
         return fastAndSlow(head);
     }
+
+    // Friday, March 21, 2025 6:02:29 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
     public ListNode fastAndSlow(ListNode head){
         if(head==null) return head;
         ListNode fast = head;
