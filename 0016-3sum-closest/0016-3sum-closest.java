229a9a4040f4f34406fc7a49c8f0d161fce6057a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-12 18:56:09 +0530

    Time: 13 ms (83.94%), Space: 43.2 MB (59.86%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,28 +1,29 @@
 // Leetcode : 16
 class Solution {
-    public int threeSumClosest(int[] nums, int target) {
+    public int threeSumClosest(int[] nums, int target) {        
+        return twoPointerApproach(nums,target);
+    }
+
+    // Saturday, April 12, 2025 6:45:03 PM
+    // Time Complexity:O(nlogn)+ O(n^2) | Space Complexity:O(1)
+    public int twoPointerApproach(int[] nums, int target) {
         int len = nums.length;
-        if(nums==null || len<3) return 0;
+        if (nums == null || len < 3) return 0;
         int closestSum = Integer.MAX_VALUE;
         Arrays.sort(nums);
-        for(int i=0;i<len;i++){
-            
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            
-            int lo = i+1;
-            int hi = len-1;
 
-            while(lo<hi){
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; //<--
+
+            int lo = i + 1;
+            int hi = len - 1;
+            while (lo < hi) {
                 int csum = nums[i] + nums[lo] + nums[hi];
-                if(Math.abs(csum-target)< Math.abs(closestSum-target)){
-                    closestSum=csum;
-                }
+                if (Math.abs(csum - target) < Math.abs(closestSum - target)) {closestSum = csum;}
 
-                if(csum<target) lo++;
-                else if(csum>target) hi--;
-                else{
-                    return csum;
-                }
+                if (csum < target) lo++;
+                else if (csum > target) hi--;
+                else {return csum;}
             }
         }
         return closestSum;
