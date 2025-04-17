53f1d1635fdf6a6140039abfc9d55d332cc9cd93
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-17 23:25:14 +0530

    Time: 1 ms (12.52%), Space: 43.2 MB (10.39%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,34 +19,35 @@ class Solution {
         return bfsIterative(root);
     }
 
-
     // Sunday, December 29, 2024 10:50:39 PM
     // Time Complexity:O(n) | Space Complexity:O(WidthOfTree)
     public int bfsIterative(TreeNode root) {
-        if(root==null) return 0;
+        if (root == null)
+            return 0;
         Queue<TreeNode> que = new LinkedList<>();
         que.offer(root);
-        int level=0;
-        while(!que.isEmpty()){
-            int size = que.size();            
-            for(int i=0;i<size;i++){
+        int level = 0;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
                 TreeNode node = que.poll();
-                if(node.left!=null) que.offer(node.left);
-                if(node.right!=null) que.offer(node.right);
+                if (node.left != null)
+                    que.offer(node.left);
+                if (node.right != null)
+                    que.offer(node.right);
             }
             level++;
         }
         return level;
     }
 
-
     // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
     // Sunday, December 29, 2024 10:51:46 PM
     public int dfsRecursive(TreeNode root) {
-        if(root==null) return 0;
-        int left=dfsRecursive(root.left);
-        int right=dfsRecursive(root.right);
-        return 1+Math.max(left,right);
+        if (root == null)
+            return 0;
+        int left = dfsRecursive(root.left);
+        int right = dfsRecursive(root.right);
+        return 1 + Math.max(left, right);
     }
-
 }
