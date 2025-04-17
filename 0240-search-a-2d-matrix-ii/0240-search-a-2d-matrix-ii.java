a1d5506345e35e4e44d0d0ae2ec08dc3119bef0f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-17 22:58:18 +0530

    Time: 6 ms (49.55%), Space: 46.2 MB (21.23%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,16 +1,12 @@
 // Leetcode : 240
 
-/**
-Can we Apply Flatten Approach ? NO
-Brute Force Search Approach 
-Binary Search Approach
-*/
-
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         return binarySearchApproach(matrix,target);        
     }
 
+    // Thursday, April 17, 2025 10:57:13 PM
+    // Time Complexity:O(m+n) | Space Complexity:O(1)
     private boolean binarySearchApproach(int[][] mat,int k){
         int row = 0, col = mat[0].length-1;
 
