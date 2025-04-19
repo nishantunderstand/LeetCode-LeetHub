28d4ea8638651f2e349b53cd30d7158ea0b1085b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-19 21:22:20 +0530

    Time: 454 ms (65.59%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -3,11 +3,11 @@
 -- Monday, February 17, 2025 12:59:41 PM
 
 SELECT 
-    Person.firstName,
-    Person.lastName,
-    Address.city,
-    Address.state 
-FROM Person
-LEFT JOIN Address  
-    USING (personId);
-
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state 
+FROM Person p
+LEFT JOIN Address a 
+    ON p.personId = a.personId
+ORDER BY firstName;
