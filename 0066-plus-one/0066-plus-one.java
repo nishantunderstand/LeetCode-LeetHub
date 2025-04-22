d741da4e80fdf10e082903cd31b79ad0f0f826d1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-22 23:26:07 +0530

    Time: 1 ms (5.54%), Space: 41.7 MB (82.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,22 +2,37 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         //return arrWithArrayListCarryReverse(digits);
-        return arrWithArray(digits);
+        //return arrWithArray(digits);
+        return arrWithArrayListCarryReverseMOD(digits);
     }
-    
-    public int[] arrWithArray(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                ++digits[i];
-                return digits;
-            }
-            digits[i] = 0;
-        }
 
-        int[] ans = new int[digits.length + 1];
-        ans[0] = 1;
-        return ans;
+
+
+    public int[] arrWithArrayListCarryReverseMOD(int[] digits) {
+        int carry = 1;
+        int n = digits.length;        
+        List<Integer> res = new ArrayList<>(n);
+        // Initialize the ArrayList with placeholder values (e.g., 0)
+        for (int i = 0; i < n; i++) {
+            res.add(0);
+        }        
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = carry + digits[i];
+            res.set(i, sum % 10); // Using set() to replace value at index i 
+            carry = sum / 10;
+        }
+    
+        if (carry != 0) {
+            res.add(0, carry);
+        }        
+        int[] resArr = new int[res.size()];
+        for (int i = 0; i < resArr.length; i++) {
+            resArr[i] = res.get(i);
+        }
+        
+        return resArr;
     }
+
 
     // Tuesday, April 22, 2025 10:36:48 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
@@ -42,4 +57,24 @@ class Solution {
         }
         return resArr;
     }
+    
+
+
+
+    // Walkccc
+    public int[] arrWithArray(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+
+    
 }
