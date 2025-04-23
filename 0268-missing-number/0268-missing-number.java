e6a893dae2cb42e25e3ff0f3c0c0c7c17cfb3be6
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-23 20:14:55 +0530

    Time: 0 ms (100%), Space: 45.6 MB (21.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,39 @@
+// Leetcode : 268
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber(int[] nums) {        
+        return mathApproach(nums);
+        //return hashSetApproach(nums);
+    }  
+
+    private int hashSetApproach(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            hs.add(nums[i]);
+        }
+
+        for(int i=0;i<=n;i++){
+            if(!hs.contains(i)) return i;
+        }
+        return -1;     
+    }
+
+
+
+    // Wednesday, April 23, 2025 8:06:30 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int mathApproach(int[] nums) {
         int n = nums.length;
         if(n==0){
             return 0;
         }
-        int totalsum = n*(n+1)/2;
-        for(int i=0;i<nums.length;i++){
-            totalsum-=nums[i];
+        int expectedSum = n*(n+1)/2;
+        int actualSum = 0;
+        for(int i=0;i<n;i++){
+            actualSum += nums[i];
         }
-        return totalsum;
+        return expectedSum-actualSum;
         
     }
 }
