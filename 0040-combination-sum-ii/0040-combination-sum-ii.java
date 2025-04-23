9dd036809c194514067f9ef5eb1acaca51c88c47
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-23 19:46:01 +0530

    Time: 2 ms (99.82%), Space: 43.7 MB (10.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,15 +1,44 @@
 // Leetcode : 40
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    List<List<Integer>> result;
+    int[] candidates;
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {        
+        this.result = new ArrayList<>();  //<--
+        this.candidates = candidates;  //<--
+
+        Arrays.sort(candidates);  
+        
+        /** 
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        Arrays.sort(candidates);
+        List<Integer> curr = new ArrayList<>();                
         backtrackingFor(candidates,target,0,res,curr);
         return res;
+        */
+
+        backtrackingFor(target,0,new ArrayList<>());
+        return result;
     }
     
+    private void backtrackingFor(int target, int idx, List<Integer> curr){
+        if(target==0){
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i=idx;i<candidates.length;i++){
+            if(i>idx && candidates[i]==candidates[i-1]) continue;  //<--
+            if(candidates[i]>target) break;  //<--
+
+            curr.add(candidates[i]);
+            backtrackingFor(target-candidates[i],i+1,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
+
+
     // Thursday, April 10, 2025 6:51:16 PM
-    // Time Complexity:O(V+En^2^nm*nlognHeightOfTree1Width) | Space Complexity:O(1V+Em*nHeightOfTreeWidth)
+    // Time Complexity:O(2^n*n) | Space Complexity:O(n)
     private void backtrackingFor(int[] candidates,int target,int idx,List<List<Integer>> res, List<Integer> curr){
         // Base Case
         if(target==0){
