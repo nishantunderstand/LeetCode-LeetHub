99bfc814a10e4a7ca6ed2d4f88be4ce02b2ad8b1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-26 20:37:48 +0530

    Time: 1 ms (5.64%), Space: 44.5 MB (12.31%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// Leetcode : 83
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        //return sortedCase(head);
+        //return unsortedCase(head);
+        return unsortedCaseDummy(head);
+    }
+
+
+    public ListNode unsortedCaseDummy(ListNode head) {
+        HashSet<Integer> seen = new HashSet<>();
+        ListNode dummy = new ListNode(0, head); // Dummy before head
+        ListNode prev = dummy, curr = head;
+
+        while (curr != null) {
+            if (seen.add(curr.val)) { // First time seeing value
+                prev = curr;
+            } else {
+                prev.next = curr.next; // Skip duplicate
+            }
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }        
+
+
+
+     private ListNode unsortedCase(ListNode head) {
+        if (head == null) return null;
+
+        HashSet<Integer> seen = new HashSet<>();
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) {
+            if (seen.contains(curr.val)) {
+                prev.next = curr.next; // Remove curr
+            } else {
+                seen.add(curr.val);
+                prev = curr; // Move prev to curr
+            }
+            curr = curr.next; // Move curr always
+        }
+
+        return head;
+    }
+
+    // Saturday, April 26, 2025 8:36:11 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    private ListNode sortedCase(ListNode head){
+        if(head==null || head.next==null) return head;        
+        ListNode curr = head;
+        while(curr!=null && curr.next!=null){
+            if(curr.val == curr.next.val){
+                curr.next = curr.next.next; // Skip
+            }else{
+                curr = curr.next; // Normal Approach
+            }
+        }
+        // return curr; <-- WRONG
+        return head;        
+    }
+}
