958af25929858fe2f9a9a8901f6e351cb2ccb933
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-26 12:01:11 +0530

    Time: 277 ms (76.76%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,22 +3,34 @@
 -- Saturday, April 19, 2025 10:02:43 PM
 
 
-/**  FAILED 
-Case: Only One Unique Salary
-Case: All Salaries Are the Same
-SELECT MAX(e.salary) AS SecondHighestSalary
-FROM Employee e
-WHERE e.salary < (SELECT MAX(e.salary) FROM Employee);
-*/
+-- Case : Only One Unique Salary
+-- Case : All Salaries Are the Same
+-- Case : Handle All Edge Case 
 
-
-SELECT
-    (SELECT DISTINCT
-    e.salary 
+SELECT 
+    MAX(e.salary) AS SecondHighestSalary
 FROM 
     Employee e
-ORDER BY e.salary DESC -- 
-LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+WHERE 
+    e.salary < (
+        SELECT 
+            MAX(salary) 
+        FROM 
+            Employee
+    );
 
 
+/**
+SELECT (
+    SELECT DISTINCT 
+        e.salary
+    FROM 
+        Employee e
+    ORDER BY 
+        e.salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+
+*/
 
