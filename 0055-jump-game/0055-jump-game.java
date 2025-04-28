6fab5043e6f479c61e23ac9ffe9ac396dd8652b9
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-28 20:46:59 +0530

    Time: 2 ms (81.7%), Space: 45.9 MB (42.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,4 @@
 // Leetcode : 55
-
 class Solution {
     public boolean canJump(int[] nums) {
         //return striverApproach(nums);
@@ -17,7 +16,8 @@ class Solution {
         return true; // Reached the end
     }
 
-
+    // Sunday, February 23, 2025 10:30:30 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
     public boolean striverApproach(int[] nums) {
         int maxReach =  0;
         int len = nums.length;
