67d3e53b49eb8f8a88bc5423ff68c15c2cd96dca
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-04-30 19:19:52 +0530

    Time: 5 ms (100%), Space: 45 MB (80.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,7 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
+// Leetcode : 235
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         //return bruteForceBT(root,p,q);
@@ -16,7 +16,7 @@ class Solution {
     }
 
     // Wednesday, January 29, 2025 6:35:36 PM
-    // Time Complexity:O(logn) | Space Complexity:O(HeighthOfTree)
+    // Time Complexity:O(H) | Space Complexity:O(HeighthOfTree)
     public TreeNode lcaCleaner(TreeNode root, TreeNode p, TreeNode q) {
         if(root.val > Math.max(p.val,q.val)) return lcaCleaner(root.left,p,q);
         if(root.val < Math.min(p.val,q.val)) return lcaCleaner(root.right,p,q);
@@ -24,22 +24,21 @@ class Solution {
     }
 
     // Wednesday, January 29, 2025 6:33:35 PM
-    // Time Complexity:O(logn) | Space Complexity:O(HeightOfTree)
+    // Time Complexity:O(H) | Space Complexity:O(H)
+    // H = height of tree, O(logN) balanced, O(N) skewed
     public TreeNode lcaBST(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null) return root;
+        if(root==null) return null;
         // Smaller
-        if(p.val <root.val && q.val < root.val){
-            return lcaBST(root.left,p,q);
-        }
+        if(p.val <root.val && q.val < root.val) return lcaBST(root.left,p,q);
+        
         // Greater
-        if(p.val >root.val && q.val > root.val){
-            return lcaBST(root.right,p,q);
-        }
+        if(p.val >root.val && q.val > root.val) return lcaBST(root.right,p,q);
+        
         return root;
     }
     
     // Saturday, January 4, 2025 6:01:39 PM
-    // Time Complexity:O(logn) | Space Complexity:O(HeightOfTree)
+    // Time Complexity:O(N) | Space Complexity:O(N)
     public TreeNode bruteForceBT(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return root;
         if(root.val==p.val || root.val==q.val) return root;
@@ -47,12 +46,14 @@ class Solution {
         TreeNode left=bruteForceBT(root.left,p,q);
         TreeNode right=bruteForceBT(root.right,p,q);
 
-        if(left!=null && right!=null){
-            return root;
-        }else if(left!=null){
-            return left;
-        }else{
-            return right;
-        }
+        /** Approach 1     
+        if(left!=null && right!=null) return root;
+        else if(left!=null)return left;
+        else return right;
+        */
+
+        // Approach 2
+        if(left!=null && right!=null) return root;
+        return (left!=null)?left:right;
     }
 }
