bc4e4d6a29b9c5c5f7b82a078d60a507d2d9b0ea
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-14 23:44:56 +0530

    Time: 3 ms (91.52%), Space: 46.7 MB (31.91%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,6 +1,12 @@
 // Leetcode : 1004
 class Solution {
-    public int longestOnes(int[] nums, int k) {
+    public int longestOnes(int[] nums, int k) {            
+        return slidingWindowApproach(nums,k);       
+    }
+
+    // Wednesday, May 14, 2025 11:44:13 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public int slidingWindowApproach(int[] nums, int k) {        
         int zeroCnt=0,windowStart = 0,maxLen=0;
         for(int windowEnd=0;windowEnd<nums.length;windowEnd++){
             if(nums[windowEnd]==0) zeroCnt++;
