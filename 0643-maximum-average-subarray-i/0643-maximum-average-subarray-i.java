5e30e7afc4e3eba44d92a2a3bf2b321feaf6d877
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-15 10:17:03 +0530

    Time: 5 ms (49.38%), Space: 56.5 MB (34.33%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -33,7 +33,7 @@ class Solution {
 
         int windowStart = 0;
         double windowSum = 0;
-        double max = 0;
+        double max = 0;  //<--
         for(int windowEnd=0;windowEnd<nums.length;windowEnd++){
             windowSum+=nums[windowEnd];
             if(windowEnd>=k-1){
