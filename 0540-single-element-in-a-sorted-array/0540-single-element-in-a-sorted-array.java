99257dc2ebe5eb3203f440839fb6498e9df06ca4
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-16 20:38:18 +0530

    Time: 0 ms (100%), Space: 51.1 MB (5.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,29 @@
+// Leetcode : 540
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        return hashingApproach(nums);
+        //return hashingApproach(nums);
         //return xorApproach(nums);
+        return binarySearchByChatGPT(nums);
+    }
+    
+
+    public int binarySearchByChatGPT(int[] nums) {       
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid = low + (high-low)/2; 
+            if(mid%2==1) mid--; //<--
+            if(nums[mid]==nums[mid+1]) low = mid+2;  //<--
+            else{
+                high = mid;  //<--
+            }
+
+        }
+        return nums[low];  //<--
     }
 
+    // Sunday, January 5, 2025 5:07:51 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
     public int xorApproach(int[] nums){
         int ans=nums[0];
         for(int i=1;i<nums.length;i++){
@@ -12,6 +32,8 @@ class Solution {
         return ans;
     }
 
+    // Sunday, January 5, 2025 5:07:45 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
     public int hashingApproach(int[] nums){
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i:nums){
