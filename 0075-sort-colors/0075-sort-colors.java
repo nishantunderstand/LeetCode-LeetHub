56ffcb7fc3261dd862531b72ec8142db589a30f1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-16 18:47:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (29.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,36 @@
+// Leetcode : 75
 class Solution {
-    public static void swap(int[] arr, int i,int j){
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    public void sortColors(int[] nums) {
+        dutchNationalFlag(nums);
     }
 
-    public void sortColors(int[] nums) {
-        int i=0;
-        int j=0;
-        int k = nums.length-1;
-        while(i<=k){
-            if(nums[i]==0){
-                swap(nums,i,j);
+    // Monday, April 28, 2025 8:49:08 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    // Two Way Partition
+    // Unstable Sort  //<--
+    // Relative Order is not maintained  //<--
+    public void dutchNationalFlag(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int k = nums.length - 1;
+        while (i <= k) {
+            if (nums[i] == 0) {
+                swap(nums, i, j);
                 i++;
-                j++;
-            }else if (nums[i]==1)
-            {
+                j++;  //<--
+            } else if (nums[i] == 1) {
                 i++;
-            }else{
-                swap(nums,i,k);
-                k--;
+            } else {
+                swap(nums, i, k);
+                k--;  //<--
             }
-            
         }
+    }
+
+    // Time Complexity:O(1) | Space Complexity:O(1)
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
