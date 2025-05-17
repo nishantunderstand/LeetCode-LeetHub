e494f810b5734063c4bb9b13f4fca3b1e40714bb
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-17 17:53:59 +0530

    Time: 33 ms (25.24%), Space: 45.4 MB (30.88%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -2,7 +2,23 @@
 class Solution {
     public int firstUniqChar(String s) {
         return linkedhashMapApproach(s);
+        //return frequencyArrayApproach(s);
+        //return hashMapApproach(s); // WRONG CODE? WHY ?
     }
+
+    // WRONG CODE
+    public int hashMapApproach(String s){
+        HashMap<Character,Integer> hm = new HashMap<>();  //<--
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            hm.put(curr,hm.getOrDefault(curr,0)+1);
+            if(hm.get(curr)==1) return i;
+        }        
+        return -1;
+    }
+
+
     // Saturday, February 8, 2025 6:01:15 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
     public int linkedhashMapApproach(String s){
@@ -18,6 +34,18 @@ class Solution {
             if(hm.get(curr)==1) return i;
         }
         
+        return -1;
+    }
+    // Saturday, May 17, 2025 5:53:24 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int frequencyArrayApproach(String s) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+            
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
+                return i;
         return -1;
     }
 }
