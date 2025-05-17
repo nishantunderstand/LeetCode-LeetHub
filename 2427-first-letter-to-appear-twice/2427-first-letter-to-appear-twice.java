62f01e18902a7bc4f418a36da69cb94cdf86fc93
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-17 17:43:54 +0530

    Time: 0 ms (100%), Space: 41.4 MB (90.74%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -1,6 +1,23 @@
 // Leetcode : 2351
 class Solution {
     public char repeatedCharacter(String s) {
+        //return freqArrayApproach(s);   
+        return hashMapApproach(s);
+    }
+
+    private char hashMapApproach(String s){
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            if(hm.get(ch)==2) return ch;
+        }        
+        return '\0';
+    }
+
+    // Saturday, May 17, 2025 5:36:20 PM
+    // Time Complexity:O(n) | Space Complexity:O(1h)
+    public char freqArrayApproach(String s) {
         int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
             int idx = s.charAt(i)-'a';
