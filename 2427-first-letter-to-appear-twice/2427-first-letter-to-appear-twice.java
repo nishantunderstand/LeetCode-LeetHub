8411c0adcdc18bd860679f58b31487d10846065f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-17 17:54:21 +0530

    Time: 0 ms (100%), Space: 41.8 MB (14.75%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -3,8 +3,29 @@ class Solution {
     public char repeatedCharacter(String s) {
         //return freqArrayApproach(s);   
         return hashMapApproach(s);
+        //return linkedhashMapApproach(s); // WHY ?? WRONG CODE
+    }
+    
+    // WRONG CODE
+    public char linkedhashMapApproach(String s){
+        LinkedHashMap<Character,Integer> hm = new LinkedHashMap<>();  //<--
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            hm.put(curr,hm.getOrDefault(curr,0)+1);
+        }
+
+        for(int i=0;i<len;i++){
+            char curr = s.charAt(i);
+            if(hm.get(curr)==2) return curr;
+        }
+        
+        return '\0';
     }
 
+
+    // Saturday, May 17, 2025 5:36:20 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
     private char hashMapApproach(String s){
         HashMap<Character,Integer> hm = new HashMap<>();
         for(int i=0;i<s.length();i++){
@@ -16,7 +37,7 @@ class Solution {
     }
 
     // Saturday, May 17, 2025 5:36:20 PM
-    // Time Complexity:O(n) | Space Complexity:O(1h)
+    // Time Complexity:O(n) | Space Complexity:O(1)
     public char freqArrayApproach(String s) {
         int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
