ec59092dcebc8a80132d746726423f4c1f87a229
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-18 22:38:22 +0530

    Time: 1 ms (99.55%), Space: 45.7 MB (13.81%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,28 +3,29 @@ class Solution {
         return greedySolution(nums);
     }
 
-
-    public int greedySolution(int[] A) {
-        int n = A.length;
+    // Sunday, May 18, 2025 10:35:57 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public int greedySolution(int[] nums) {
+        int n = nums.length;
 
         if (n <= 1) return 0;
-        if (A[0] == 0) return -1;
+        if (nums[0] == 0) return -1;  //<--
 
         int jumps = 0;
         int currentEnd = 0;
-        int farthest = 0;
+        int maxReach = 0;
 
         for (int i = 0; i < n - 1; i++) {
-            farthest = Math.max(farthest, i + A[i]);
+            maxReach = Math.max(maxReach, i + nums[i]);
 
             if (i == currentEnd) {
                 jumps++;
-                //if(farthest == currentEnd) return -1;
-                currentEnd = farthest;
+                if(maxReach == currentEnd) return -1;  //<--
+                currentEnd = maxReach; //<--
             }
         }
 
-        return currentEnd >= n - 1 ? jumps : -1;
+        return currentEnd >= n - 1 ? jumps : -1;  //<--
     }
 
 }
