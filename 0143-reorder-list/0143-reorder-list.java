16dc8a9af41ababa59c59b6df3cb9d3adaeef12f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-18 17:21:08 +0530

    Time: 2 ms (86.37%), Space: 48.7 MB (15.54%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,57 @@
+// Leetcode : 143
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // Sunday, May 18, 2025 5:20:29 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null) return;
+        ListNode mid = findMiddle(head);        
+        ListNode second = reverse(mid.next);
+        mid.next = null;
+        ListNode first = head;
+        merge2LL(first,second);
+    }
+    private void merge2LL(ListNode first, ListNode second){                
+        while(first!=null && second!=null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+
+    private ListNode findMiddle(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null) {
+            ListNode nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+}
