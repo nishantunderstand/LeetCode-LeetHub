4cdd905a7e4589fe99ee8f0a0d65f073c5758713
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-18 21:13:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (21.48%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,9 +1,33 @@
 // Leetcode : 153
 class Solution {
     public int findMin(int[] nums) {       
-        return findMinBS(nums);
+        //return findMinBS(nums);
+        return binarySearchLike33(nums);
     }
 
+    private int binarySearchLike33(int[] nums){
+        int lo = 0,hi = nums.length-1;
+        int minVal = Integer.MAX_VALUE;
+
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            // Already Sorted
+            if(nums[lo]<=nums[hi]){
+                minVal = Math.min(minVal,nums[lo]);
+                return minVal;  //<--
+            }
+
+            // left Sorted
+            if(nums[lo]<=nums[mid]){
+                minVal = Math.min(minVal,nums[lo]);
+                lo = mid+1;  //<-- 
+            }else{ // Right half Sorted 
+                minVal = Math.min(minVal,nums[mid]);
+                hi = mid-1;  //<--
+            }
+        }
+        return -1;
+    }
 
     public int findMinBS(int[] nums) {
         int low=0;
