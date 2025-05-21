7ffec754a001960fa10e3857428a666a8e89f550
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-05-21 21:49:50 +0530

    Time: 2 ms (98.06%), Space: 43.5 MB (62.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,31 @@
+// Leetcode : 242
 class Solution {
     public boolean isAnagram(String s, String t) {
+       //return bruteForceApproach(s,t);
+       return freqArrayApproach(s,t);
+    }
+
+    // Wednesday, May 21, 2025 9:48:29 PM
+    // Time Complexity:O(n) | Space Complexity:O(26)
+    private boolean freqArrayApproach(String s, String t) {
+        if(s.length()!=t.length()) return false;
+
+        int[] freqArr = new int[26];
+        for(char ch : s.toCharArray()){
+            freqArr[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            freqArr[ch-'a']--;
+        }
+        for(int i=0;i<freqArr.length;i++){
+            if(freqArr[i]!=0) return false;
+        }
+        return true;
+    }
+
+    // Wednesday, May 21, 2025 9:48:29 PM
+    // Time Complexity:O(n + nlogn) | Space Complexity:O(n)
+    public boolean bruteForceApproach(String s, String t) {
         if(s.length()!=t.length()){
             return false;
         }
