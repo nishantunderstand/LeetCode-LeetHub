3978ab4aca7578d01c8b1b88ddf4c39cae7e8c68
NishantRaj <nishant.read@gmail.com>
2025-06-30 19:59:27 +0530

    LenovoBat 30-Jun-25 19:59:27.87

---
+++ !LeetCode-LeetHubSync.bat
@@ -0,0 +1,13 @@
+echo LeetCode-LeetHub-Begin
+	set "repoLocation=C:\Users\User\eclipse-workspace\LeetCode-LeetHub"
+	set "gitPath=C:\Program Files\Git\cmd\git.exe"
+	cd /d "%repoLocation%"
+	if not exist "%repoLocation%\.git" (
+		"%gitPath%" init
+	)
+	"%gitPath%" pull
+	"%gitPath%" add .
+	set commitMessage=LenovoBat %date% %time%
+	"%gitPath%" commit -m "%commitMessage%"
+	"%gitPath%" push
+echo LeetCode-LeetHub-End
