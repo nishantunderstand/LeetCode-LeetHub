c6d75cf0a62b1a89acb44be2b7d7a31be45a0a9e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-06 23:04:13 +0530

    Time: 4579 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,8 +1,16 @@
-# Write your MySQL query statement below
 -- Leetcode : 1757
+/** Normal Approach
 SELECT 
     p.product_id
 FROM Products p
 WHERE p.low_fats='Y' AND 
     p.recyclable='Y'
+ORDER BY product_id;
+*/
+
+SELECT product_id 
+FROM 
+(SELECT * FROM Products WHERE low_fats = 'Y')
+Products
+WHERE recyclable = 'Y'
 ORDER BY product_id;
