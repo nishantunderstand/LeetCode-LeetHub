764afc5158623f6bb610cfc291fbeb5207db3737
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-26 19:47:03 +0530

    Time: 264 ms (68.97%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,12 +1,32 @@
-# Write your MySQL query statement below
 -- LeetCode : 176
 -- Saturday, April 19, 2025 10:02:43 PM
+
+/**
+❌ Rejected Approach 
+SELECT salary AS SecondHighestSalary
+FROM Employee e 
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+
+❌ Rejected Approach 
+SELECT DISTINCT salary AS SecondHighestSalary
+FROM Employee e 
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+*/
+
+SELECT (
+    SELECT DISTINCT e.salary
+    FROM Employee e
+    ORDER BY e.salary DESC 
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
 
 
 -- Case : Only One Unique Salary
 -- Case : All Salaries Are the Same
 -- Case : Handle All Edge Case 
-
+/**
 SELECT 
     MAX(e.salary) AS SecondHighestSalary
 FROM 
@@ -18,19 +38,4 @@ WHERE
         FROM 
             Employee
     );
-
-
-/**
-SELECT (
-    SELECT DISTINCT 
-        e.salary
-    FROM 
-        Employee e
-    ORDER BY 
-        e.salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
-
-
 */
-
