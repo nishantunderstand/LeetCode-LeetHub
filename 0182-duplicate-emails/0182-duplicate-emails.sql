c7e69cd933d2572206afac43ac6e438b73515e1b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-27 21:12:44 +0530

    Time: 380 ms (46.85%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,20 @@
 -- Sunday, July 27, 2025 8:59:36 PM
 -- LeetCode : 182
 
+
+/**
+-- Group By + Having Approach
 SELECT email
 FROM Person
 GROUP BY email
 HAVING COUNT(*)>1
 ORDER BY email;
+
+*/
+
+
+-- Joins
+SELECT DISTINCT p1.email
+FROM Person p1, Person p2
+WHERE p1.email = p2.email AND p1.id != p2.id
+ORDER BY p1.email;
