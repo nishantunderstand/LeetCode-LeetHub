77a2ca11ec916a549d9141916151b55482c78f96
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-27 14:48:08 +0530

    Time: 534 ms (66.14%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,8 +1,21 @@
 -- Sunday, July 27, 2025 2:38:42 PM
 -- LeetCode : 183
 
+/**
+✅ Accepted Approach
 SELECT name AS Customers
 FROM Customers 
 WHERE id NOT IN (
     SELECT customerId FROM Orders
 );
+*/
+
+
+
+-- Join Approach
+SELECT name AS Customers
+FROM Customers cus
+LEFT JOIN Orders od
+ON cus.id = od.customerId
+WHERE od.customerId IS NULL;
+
