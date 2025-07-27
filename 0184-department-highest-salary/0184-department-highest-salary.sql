05d2f8ab957d6232d2af04c8ceed970b7566d2be
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-27 16:26:08 +0530

    Time: 1200 ms (8.47%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,8 +1,9 @@
 -- Sunday, July 27, 2025 3:36:57 PM
 -- LeetCode : 184
 
--- Window Function
 
+/**
+-- Equi Join + Window Function
 SELECT 
     dep.name AS Department,
     emp.name AS Employee,
@@ -13,3 +14,19 @@ FROM (
 JOIN Department dep
 ON emp.departmentId = dep.id
 WHERE emp.rnk=1;
+*/
+
+
+
+SELECT 
+    dep.name AS Department,
+    emp.name AS Employee,
+    emp.salary AS Salary
+FROM Employee emp
+JOIN Department dep 
+ON emp.departmentId = dep.id
+WHERE emp.salary = (
+    SELECT MAX(Salary)
+    FROM Employee
+    WHERE departmentId = emp.departmentId
+);
