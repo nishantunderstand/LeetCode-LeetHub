d4508981f1ca2f2dce60432a6c1b6f7fd5596627
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-07-27 16:07:42 +0530

    Time: 807 ms (39.84%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,15 @@
+-- Sunday, July 27, 2025 3:36:57 PM
+-- LeetCode : 184
+
+-- Window Function
+
+SELECT 
+    dep.name AS Department,
+    emp.name AS Employee,
+    emp.salary AS Salary
+FROM (
+    SELECT *, RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk FROM employee 
+    ) emp
+JOIN Department dep
+ON emp.departmentId = dep.id
+WHERE emp.rnk=1;
