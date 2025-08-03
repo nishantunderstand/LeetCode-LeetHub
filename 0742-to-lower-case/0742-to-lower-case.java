301a76815af84ff83a3fcc940d1a510a7778e13c
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-08-03 16:42:10 +0530

    Time: 1 ms (21.66%), Space: 41.5 MB (78.86%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,13 @@
+// Sunday, August 3, 2025 2:29:52 PM
+// LeetCode : 709
+class Solution {
+    public String toLowerCase(String s) {
+        return inBuildMethod(s);
+        //return charBycharChange(s); // TBD
+    }
+
+    
+    public String inBuildMethod(String s) {
+        return s.toLowerCase();
+    }
+}
