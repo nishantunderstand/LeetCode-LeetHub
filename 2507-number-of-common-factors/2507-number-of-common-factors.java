e8bd2678e63d8c728fa1204e4bd5b8d04d139650
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-09-13 21:30:00 +0530

    Time: 0 ms (100%), Space: 40.6 MB (31.11%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.java
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -1,10 +1,15 @@
 class Solution {
     public int commonFactors(int a, int b) {
+        return optimizedApproach(a,b);
+    }
+
+    // Saturday, September 13, 2025 9:26:25 PM
+    // Time Complexity:O(Min(a,b)) | Space Complexity:O(1)
+    public int optimizedApproach(int a, int b) {
         if(a==0 ||b ==0){
             return 0;
         }
-        int maxDiv = Math.min(a,b);
-        
+        int maxDiv = Math.min(a,b);        
         int cnt = 0;
         for(int div=1;div<=maxDiv;div++){
             if(a%div==0 && b%div==0){
