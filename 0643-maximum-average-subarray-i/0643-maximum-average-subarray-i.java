ba38a7c6c1bc281888da837037021cf3dffa797e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-09-18 21:36:30 +0530

    Time: 6 ms (16.74%), Space: 55.8 MB (91.83%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -2,16 +2,17 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         //return bruteForce(nums,k);
-        //return slidingWindowCaterpillarWRONG(nums,k);
-        return runningSum(nums,k);
+        return slidingWindowCaterpillar(nums,k);
+        //return runningSum(nums,k);
     }
     // Postive & Negative
     // Friday, February 14, 2025 12:16:10 AM
     // Time Complexity:O(n) | Space Complexity:O(1)
+    // Two Pass Solution // <--
     public double runningSum(int[] nums, int k) {
         int len = nums.length;
         if(nums==null || nums.length==0 || k>nums.length) return 0.0;
-        double windowSum = 0;
+        double windowSum = 0; // <--
         for(int i=0;i<k;i++){
             windowSum+=nums[i];
         }
@@ -26,15 +27,15 @@ class Solution {
     }
 
 
-    // nums = [-1] k=1 FAILED
-    public double slidingWindowCaterpillarWRONG(int[] nums, int k) {
-        int len = nums.length;
-        //if(len==1)
-        //double max = Double.MIN_VALUE; //WRONG ;
-
+    // Thursday, September 18, 2025 9:34:23 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    // One Pass Solution  // <--
+    public double slidingWindowCaterpillar(int[] nums, int k) {        
+        double max = Double.NEGATIVE_INFINITY; // <--
+        
         int windowStart = 0;
-        double windowSum = 0;
-        double max = 0;  //<--
+        double windowSum = 0.0;
+        
         for(int windowEnd=0;windowEnd<nums.length;windowEnd++){
             windowSum+=nums[windowEnd];
             if(windowEnd>=k-1){
@@ -46,12 +47,11 @@ class Solution {
         return max;        
     }
 
-    // WRONG
+    // TLE 
     public double bruteForce(int[] nums, int k) {
-        int len = nums.length;
-        
+        int len = nums.length;        
         //if(nums=0||len=0||k>len) return 0.0;
-        double max = Double.MIN_VALUE;
+        double max = Double.NEGATIVE_INFINITY;
         for(int i=0;i<len;i++){
             double csum=0;
             int cnt=0;
