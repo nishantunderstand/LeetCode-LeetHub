59c0c05f22ce82bdfa6613d0022ae72fb86b2b76
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-09-21 10:23:39 +0530

    Time: 0 ms (100%), Space: 43.5 MB (17.42%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,15 +13,36 @@
  *     }
  * }
  */
-
-// Friday, February 7, 2025 7:58:18 PM
-// Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
+// LeetCode : 112
+/**
+| Bitwise OR  : No Short Circuit, Evaluate Both Side
+|| Logical OR : Short-Circuit 
+*/
 class Solution {
+    
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfsApproach1(root,targetSum);
+        //return hasPath(root,targetSum);
+    }
+
+    // Friday, February 7, 2025 7:58:18 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)    
+    public boolean dfsApproach1(TreeNode root, int targetSum) {
         if(root==null) return false;
         if(root.left==null && root.right==null && root.val == targetSum) return true;
         boolean leftSum = hasPathSum(root.left,targetSum-root.val);
         boolean rightSum = hasPathSum(root.right,targetSum-root.val);
-        return leftSum|rightSum; //<- BITWISE OR 
+        //return leftSum | rightSum; //<- BITWISE OR // WRONG 
+        return leftSum || rightSum; //<- Logical OR // Correct
     }
+
+
+    public static boolean hasPath(TreeNode root, int sum){			
+		if(root==null) return false;
+		int remainingSum = sum - root.val;		
+		// Leaf Node 
+		if(root.left==null && root.right==null) return remainingSum==0;
+		// Child Node
+		return hasPath(root.left,remainingSum) || hasPath(root.right,remainingSum);
+	}
 }
