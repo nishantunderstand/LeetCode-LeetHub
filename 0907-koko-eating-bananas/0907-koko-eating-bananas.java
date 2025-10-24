1a25f09a925feb622dadd48915aa2197d9374a7a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-25 00:59:20 +0530

    Time: 6 ms (99.36%), Space: 45.3 MB (47.13%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,6 +1,12 @@
 // LeetCode 875
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        return canKokoEat(piles,h);
+    }
+
+    // Saturday, October 25, 2025 12:57:00 AM
+    // Time Complexity:O(n*log m) n : piles lengths m:maxpiles size | Space Complexity:O(1)
+    public int canKokoEat(int[] piles, int h) {
         int minSpeed = 1;
         int maxSpeed = 0;
 
@@ -19,19 +25,18 @@ class Solution {
         }
         return minSpeed;
     }
+
     public boolean canEat(int speed,int [] piles,int h){
         int hours = 0;         
         for(int pile : piles ){
-            hours += (pile+speed-1)/speed;
+            hours += (pile+speed-1)/speed;  //<--
         }
         return hours<=h;
     }
-
-
     public boolean canEatDoubleVersion(int speed,int [] piles,int h){
         int hours = 0;         
         for(int pile : piles ){
-            hours += (int) Math.ceil((double) pile/speed);
+            hours += (int) Math.ceil((double) pile/speed);  //<--
         }
         return hours<=h;
     }
