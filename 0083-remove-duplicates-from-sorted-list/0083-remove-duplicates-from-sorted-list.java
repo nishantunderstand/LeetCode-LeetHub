aee5060a8dd9c4663fad95f5b4626144a296fe97
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-24 23:27:22 +0530

    Time: 1 ms (4.99%), Space: 44.4 MB (22.04%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -85,9 +85,11 @@ class Solution {
         ListNode curr = head;
         while(curr!=null && curr.next!=null){
             if(curr.val == curr.next.val){
-                curr.next = curr.next.next; // Skip
+                // Skip Duplicate
+                curr.next = curr.next.next; 
             }else{
-                curr = curr.next; // Normal Approach
+                // Move to Next Node
+                curr = curr.next; 
             }
         }
         // return curr; <-- WRONG
