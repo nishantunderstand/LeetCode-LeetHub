af57791ca518bb7c3b705892040af61644b36684
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-25 10:50:15 +0530

    Time: 0 ms (100%), Space: 46.4 MB (9.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,11 +1,55 @@
 // Leetcode : 34
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        
+    public int[] searchRange(int[] nums, int target) {        
+        //return standardApproach(nums, target);        
+        return binarySearchApproach(nums, target);        
+    }
+    
+    // Saturday, October 25, 2025 10:49:18 AM
+    // Time Complexity:O(nlogn) | Space Complexity:O(1)
+    public int[] binarySearchApproach(int[] nums, int target) {       
+        int first = lowerBound(nums,target);
+        int last = upperBound(nums,target)-1;  //<--
+
+        // Check for valid result 
+        if(first==nums.length || nums[first]!=target){
+            return new int[]{-1,-1};
+        }       
+        return new int[]{first,last};
+    } 
+
+    public int lowerBound(int[] nums, int target){
+        int left=0,right=nums.length;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]<target){  //<--
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public int upperBound(int[] nums, int target){
+        int left=0,right=nums.length;
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]<=target){  //<--
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+    
+    // Wednesday, April 23, 2025 7:01:04 PM
+    // Time Complexity:O(nlogn) | Space Complexity:O(1)
+    public int[] standardApproach(int[] nums, int target) {        
         if(nums==null||nums.length==0){
             return new int[]{-1,-1};
         }
-        
         // Finding 1st
         int first = -1;
         int lo = 0;
