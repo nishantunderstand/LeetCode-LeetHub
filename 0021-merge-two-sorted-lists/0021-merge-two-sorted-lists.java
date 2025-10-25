80e24f3e24d0c723b6a2590e454421c47a2e245b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-26 01:33:00 +0530

    Time: 0 ms (100%), Space: 42.4 MB (76.66%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// LeetCode : 21
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         //return iterativeMerge(list1,list2);
@@ -20,11 +21,11 @@ class Solution {
         if(list1==null) return list2;
         if(list2==null) return list1;
             if(list1.val<list2.val){
-                list1.next=recursiveMerge(list1.next,list2);//<--
+                list1.next=recursiveMerge(list1.next,list2); //<--
                 return list1; //<--
             }else{
-                list2.next=recursiveMerge(list1,list2.next);//<--
-                return list2;//<--
+                list2.next=recursiveMerge(list1,list2.next); //<--
+                return list2; //<--
             }
     }
 
@@ -33,8 +34,10 @@ class Solution {
     public ListNode iterativeMerge(ListNode list1, ListNode list2) {
         if(list1==null) return list2;
         if(list2==null) return list1;
-        ListNode dummyNode = new ListNode(-1);
-        ListNode current = dummyNode;
+
+        ListNode dummyNode = new ListNode(-1); 
+        ListNode current = dummyNode; //<--
+        
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
                 current.next=list1; 
@@ -46,15 +49,10 @@ class Solution {
             current=current.next;
         }
         /**
-        if(list1!=null){
-            current.next=list1;
-        }
-        if(list2!=null){
-            current.next=list2;
-        }
+        if(list1!=null){current.next=list1;}
+        if(list2!=null){current.next=list2;}
         */
         current.next=(list1!=null)?list1:list2;
-
-        return dummyNode.next;
+        return dummyNode.next; //<--
     }
 }
