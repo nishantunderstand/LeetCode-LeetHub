a9ba54e14fe1a63d50b7420883019d8576b1ba5c
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-26 01:18:33 +0530

    Time: 2 ms (51.53%), Space: 41.8 MB (7.87%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -1,7 +1,43 @@
 // LeetCode : 1748
 class Solution {
     public int sumOfUnique(int[] nums) {        
-        return hashMapApproach(nums);
+        //return hashMapApproach(nums);
+        //return singleHashSetApproach(nums);
+        return twoHashSetApproach(nums);
+    }
+    // Sunday, October 26, 2025 1:17:21 AM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private int twoHashSetApproach(int[] nums){
+        Set<Integer> unique = new HashSet<>();
+        Set<Integer> duplicate = new HashSet<>();
+
+        for(int num : nums){
+            if(!unique.add(num)){
+                duplicate.add(num);
+            }
+        }
+        // Remove Duplicate
+        unique.removeAll(duplicate);
+        int csum=0;
+        for(int num:unique){
+            csum += num;
+        }
+        return csum;
+    }
+
+
+    // FAILED [1,1,1,1,1]
+    private int singleHashSetApproach(int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        int csum=0;
+        for(int i=0;i<nums.length;i++){
+            if(!seen.add(nums[i])){
+                csum -= nums[i]; // AlreadySeen , Subtract From Sum
+            }else{
+                csum += nums[i]; // First Time Present
+            }
+        }
+        return csum;
     }
 
     // Sunday, October 26, 2025 12:59:47 AM
@@ -14,7 +50,7 @@ class Solution {
         for(int i=0;i<n;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-           
+
         int csum=0;
         for(int ikey : hm.keySet()){
             if(hm.get(ikey)==1){ // I want to compare Key should be 1
