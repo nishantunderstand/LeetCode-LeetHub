7387cdc5c5ec4238e1aa9024634f72b7827433c9
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-26 01:01:24 +0530

    Time: 2 ms (51.91%), Space: 41.6 MB (33.53%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -1,21 +1,20 @@
-/**
-I create HashMap
-I need Element with Freq ==1
-Edge Case 0 1
-*/
+// LeetCode : 1748
 class Solution {
-    public int sumOfUnique(int[] nums) {
+    public int sumOfUnique(int[] nums) {        
+        return hashMapApproach(nums);
+    }
+
+    // Sunday, October 26, 2025 12:59:47 AM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private int hashMapApproach(int[] nums) {
         int n = nums.length;
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return nums[0];
-        }
+        if(n==0){return 0;}
+        if(n==1){return nums[0];}
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0;i<n;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
+           
         int csum=0;
         for(int ikey : hm.keySet()){
             if(hm.get(ikey)==1){ // I want to compare Key should be 1
