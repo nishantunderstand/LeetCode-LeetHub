34c54976b01af73d3860c6b6e53bad97478f1dc6
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-10-26 14:18:07 +0530

    Time: 0 ms (100%), Space: 49.4 MB (29.57%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,5 +1,12 @@
+// LeetCode : 344
 class Solution {
     public void reverseString(char[] s) {
+        inplaceApproach(s);
+    }
+    
+    // Sunday, October 26, 2025 2:17:14 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public void inplaceApproach(char[] s) {
         int low = 0;
         int high = s.length - 1;
         while(low<=high){
