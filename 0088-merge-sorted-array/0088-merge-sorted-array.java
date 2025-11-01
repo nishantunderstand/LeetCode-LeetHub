4d8e1532d457c0215043bc86ea5aea3f297c79a1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-11-01 14:33:35 +0530

    Time: 1 ms (26.95%), Space: 44 MB (8.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,22 @@
+// LeetCode : 88
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        bruteForce(nums1,m,nums2,n); //<--
+        // bruteForce(nums1,m,nums2,n); //<--
+        priorityQueue(nums1,m,nums2,n);
+    }  
+
+    private void priorityQueue(int[] nums1,int m, int[] nums2,int n){
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        for(int i=0;i<m;i++){
+            minPQ.offer(nums1[i]);
+        }
+        for(int i=0;i<n;i++){
+            minPQ.offer(nums2[i]);
+        }        
+        int idx=0;
+        while(minPQ.size()!=0){
+            nums1[idx++]=minPQ.poll();
+        }  
     }
 
     // Add all nums2 element to  nums1, Then Sort it
