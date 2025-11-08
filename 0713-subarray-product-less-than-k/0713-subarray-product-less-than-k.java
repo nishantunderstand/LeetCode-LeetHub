840acc97e38aba08dc183eb1ef7cfc9902314135
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-11-08 20:55:44 +0530

    Time: 4 ms (99.86%), Space: 51.2 MB (6%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,10 +1,36 @@
+// Leetcode : 713
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        return dynamicWindow(nums,k);
+        return dynamicSlidingWindow(nums,k);
         //return hashMapApproach(nums,k);
+    
     }
 
-    // PENDING 
+    // Saturday, November 8, 2025 8:54:06 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    public int dynamicSlidingWindow(int[] nums, int k) {
+        // Dynamic Window
+        int windowProd=1;
+        int startIdx=0;
+        int cnt = 0;
+        int len = nums.length;
+        for(int windowEnd=0;windowEnd<len;windowEnd++){
+            windowProd *=nums[windowEnd];
+            // Shrink the Window if product exceed k 
+            while(windowProd>=k && startIdx<=windowEnd){  //<-- Out of Bound
+                windowProd /= nums[startIdx];
+                startIdx++;
+            }
+            cnt+=(windowEnd-startIdx+1);   //<--
+        }
+        return cnt;
+    }
+
+
+
+    
+    // A HashMap is typically useful for prefix sum problems, where we check:
+    // Why not HashMap? Products are multiplicative → cannot use prefix sum hashmap directly.
     public int hashMapApproach(int[] nums, int k) {
         HashMap<Long,Integer> hm = new HashMap<>();
         hm.put(1L,1);
@@ -14,26 +40,6 @@ class Solution {
         for(int i=0;i<len;i++){
             prod *=nums[i];
 
-        }
-        return cnt;
-    }
-
-    // Friday, February 7, 2025 11:25:11 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
-    public int dynamicWindow(int[] nums, int k) {
-        // Dynamic Window
-        int windowProd=1;
-        int startIdx=0;
-        int cnt = 0;
-        int len = nums.length;
-        for(int i=0;i<len;i++){
-            windowProd *=nums[i];
-            // Shrink the Window if product exceed k 
-            while(windowProd>=k && startIdx<=i){  //<-- Out of Bound
-                windowProd /= nums[startIdx];
-                startIdx++;
-            }
-            cnt+=(i-startIdx+1);  // Why are we counting like this ?          
         }
         return cnt;
     }
