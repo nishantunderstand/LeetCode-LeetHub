02b219fb9345340f778d10144735b8bc1eebe03d
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-11-23 00:06:01 +0530

    Time: 1 ms (17.38%), Space: 42.8 MB (20.3%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,28 @@
+// LeetCode : 709
+class Solution {
+    public String toLowerCase(String s) {
+        // return inBuildMethod(s);
+        return charBycharChange(s);
+    }
+
+    // Sunday, November 23, 2025 12:04:52 AM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private String charBycharChange(String str){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            if(curr>='A' && curr<='Z'){
+                curr = (char)(curr+32);  //<--                
+            }
+            sb.append(curr);  //<--
+        }
+        return sb.toString();
+    }
+    
+    // Sunday, August 3, 2025 2:29:52 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)    
+    public String inBuildMethod(String s) {
+        return s.toLowerCase();
+    }
+}
