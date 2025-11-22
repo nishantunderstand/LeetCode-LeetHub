0db157db8030eee626cfab84c20f2cc2954bd533
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-11-22 11:42:51 +0530

    Time: 4 ms (5.46%), Space: 46.7 MB (18.54%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -17,31 +17,29 @@
 // LeetCode : 230
 class Solution {
     int ans=0;
-    int cnt=0;
-    // Thursday, January 2, 2025 2:24:34 PM
-    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
+    int cnt=0;        
     public int kthSmallest(TreeNode root, int k) {
        bstIterativeApproach(root,k); 
        //return ans;
        
-       PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-       bstHeapApproach(root,k,pq);
-       return pq.peek();
+       PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+       bstHeapApproach(root,k,maxHeap);
+       return maxHeap.peek();
     }
 
-    // Does it Matter? Min or Max ?
-    // Pre/Post/In-Order
-    private void bstHeapApproach(TreeNode node, int k,PriorityQueue<Integer> pq){
-        if(node==null) return;        
-        
-        pq.offer(node.val);
-        if(pq.size()>k) pq.poll();
-        
-        bstHeapApproach(node.left,k,pq);
-        bstHeapApproach(node.right,k,pq);    
+    // Pre/Post/In-Order : Doesn't Matter  //<--
+    // Saturday, November 22, 2025 11:40:14 AM
+    // Time Complexity:O(n logk) | Space Complexity:O(k+HeightOfTree)
+    private void bstHeapApproach(TreeNode node, int k,PriorityQueue<Integer> maxHeap){
+        if(node==null) return;                                
+        bstHeapApproach(node.left,k,maxHeap);            
+        bstHeapApproach(node.right,k,maxHeap);         
+        maxHeap.offer(node.val);
+        if(maxHeap.size()>k) maxHeap.poll();   
     }
-
-    // InOrder as it is Sorted
+    
+    // Thursday, January 2, 2025 2:24:34 PM
+    // Time Complexity:O(n) | Space Complexity:O(HeightOfTree)
     public void bstIterativeApproach(TreeNode root, int k) {
         if(root==null||k==0) return;
         bstIterativeApproach(root.left,k);
