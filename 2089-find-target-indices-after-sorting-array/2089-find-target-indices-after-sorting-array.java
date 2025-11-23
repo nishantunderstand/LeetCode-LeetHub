1a32fb1ce80894de8d83778184875f81cb272304
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-11-23 13:45:13 +0530

    Time: 5 ms (27.44%), Space: 46.6 MB (14.13%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -0,0 +1,33 @@
+// Leetcode : 2089
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {        
+        //return bySorting(nums,target);
+        
+        Arrays.sort(nums);  //<--
+        List<Integer> res = new ArrayList<>();
+        recursiveApproach(nums,target,0,res);
+        return res;
+    }
+
+    private void recursiveApproach(int[] nums, int target, int index, List<Integer> res){
+        if(index == nums.length) return;
+        if(nums[index]==target) res.add(index);
+        recursiveApproach(nums,target,index+1,res);
+
+    }
+
+    // Friday, May 16, 2025 4:58:23 PM
+    // Time Complexity:O(nlogn) | Space Complexity:O(n)
+    public List<Integer> bySorting(int[] nums, int target) {
+        List<Integer> res = new ArrayList<>();
+        if(nums==null || nums.length==0) return res;        
+        
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
