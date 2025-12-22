2676198387d71f49252f51ad17742f35e28c2886
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-22 21:47:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,7 +2,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         return inplaceApproach(nums);
-        
+            
     }
 
     // Wednesday, April 23, 2025 6:32:02 PM
@@ -11,13 +11,13 @@ class Solution {
         if (nums.length <= 0) {
             return 0;
         }
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int idx = 1;  //<--
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {                
+                nums[idx] = nums[i];
+                idx++  //<--
             }
         }
-        return i+1;
+        return idx;
     }
 }
