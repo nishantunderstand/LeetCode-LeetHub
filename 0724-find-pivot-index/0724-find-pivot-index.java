0f3e5967cc7b7fe98e214aa08e02f5042ef2b3db
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-25 16:56:49 +0530

    Time: 1 ms (97.14%), Space: 47.4 MB (48.09%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,9 +1,67 @@
 // Leetcode : 724
 class Solution {
     public int pivotIndex(int[] nums) {
-        return modifiedRunningSum(nums);
+        //return modifiedRunningSum(nums);
         //return runningSum(nums);
+        //return runningSum2(nums);
+        //return prefixSumApproach(nums);
+        return suffixSumApproach(nums);
+        
     }
+
+    private int suffixSumApproach(int[] nums){
+        int n = nums.length;
+        
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        int[] suffix = new int[n];
+        suffix[n-1] = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = suffix[i+1] + nums[i];
+        }
+
+        for(int i=0;i<n;i++){
+            int leftSum = (i==0)?0:prefix[i-1];
+            int rightSum = (i==n-1)?0:suffix[i+1];
+            if(leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+
+    private int prefixSumApproach(int[] nums){
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        for(int i=0;i<n;i++){
+            int leftSum = (i==0)?0:prefix[i-1];
+            int rightSum = prefix[n-1] - prefix[i]; 
+            if(leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+
+    private int runningSum2(int[] nums) {
+        int totalSum = 0;
+        for(int num: nums){
+            totalSum += num;
+        }       
+        int leftSum = 0;
+        for(int i=0;i<nums.length;i++){
+            int rightSum =  totalSum - leftSum - nums[i];
+            if(leftSum==rightSum) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+   
 
     // Monday, March 17, 2025 9:11:46 PM
     // Time Complexity:O(n) | Space Complexity:O(1)
