4836e0459998db223e02cc73c295cc05bb01230f
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-25 15:33:05 +0530

    Time: 0 ms (100%), Space: 80.1 MB (64.47%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,8 +1,13 @@
 // LeetCode : 852
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+       return binarySearchApproach(arr);
+    }
+    // Thursday, December 25, 2025 3:31:49 PM
+    // Time Complexity:O(logn) | Space Complexity:O(1)
+    public int binarySearchApproach(int[] arr) {
         int start = 0;
-        int end = arr.length-1;
+        int end = arr.length-2;  //<--
         while(start<end){
             int mid = start + (end-start)/2;
             // Increasing Slope
