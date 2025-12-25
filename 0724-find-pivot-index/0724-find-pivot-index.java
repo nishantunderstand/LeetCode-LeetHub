746248c5372bf30398676347ad09dddc8ab0168e
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-25 16:09:02 +0530

    Time: 1 ms (97.14%), Space: 47.7 MB (6.12%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -8,16 +8,16 @@ class Solution {
     // Monday, March 17, 2025 9:11:46 PM
     // Time Complexity:O(n) | Space Complexity:O(1)
     private int runningSum(int[] nums) {
-        int tSum = 0;
+        int totalSum = 0;
         for(int num:nums){
-            tSum += num;
+            totalSum += num;
         }
-        int lSum = 0;
+        int leftSum = 0;
         // Iterate with Index
         for(int i=0;i<nums.length;i++){
-            tSum -= nums[i];
-            if(lSum==tSum) return i;  //<--
-            lSum += nums[i];
+            totalSum -= nums[i];
+            if(leftSum==totalSum) return i;  //<--
+            leftSum += nums[i];
         }
         return -1;
     }
@@ -29,19 +29,19 @@ class Solution {
         if(nums==null||n==0){
             return -1;
         }
-        int tsum = 0;
-        int lsum = 0;
+        int totalSum = 0;
+        int leftSum = 0;
         for(int i=0;i<n;i++){
-            tsum+=nums[i];
+            totalSum+=nums[i];
         }
-        // Here we are increasing lsum and also decreading tsum
-        // Alternative could be lsum == tsum-lsum-nums[i], only add nums[i] to lsum
+        // Here we are increasing lsum and also decreading totalSum
+        // Alternative could be lsum == totalSum-lsum-nums[i], only add nums[i] to lsum
         for(int i=0;i<n;i++){
-            if(lsum+nums[i] == tsum){
+            if(leftSum+nums[i] == totalSum){
                 return i;
             }
-            lsum+=nums[i];
-            tsum-=nums[i];
+            leftSum+=nums[i];
+            totalSum-=nums[i];
         }
         return -1;
     }
