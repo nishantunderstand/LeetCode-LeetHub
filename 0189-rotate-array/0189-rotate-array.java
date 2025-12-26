20ece6d3d1ba71fa1794f34bd8db45d6b8fde299
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-26 22:06:10 +0530

    Time: 0 ms (100%), Space: 61.2 MB (47.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,35 @@
 // Leetcode : 189
 class Solution {
     public void rotate(int[] nums, int k) {
-       jugglingAlgo(nums,k);
+       reversalAlgo(nums,k);
+       //reversalAlgo2(nums,k);
     }
-
     // Monday, March 17, 2025 8:28:09 AM
     // Time Complexity:O(n) | Space Complexity:O(1)
-    private void jugglingAlgo(int[] nums, int k) {
-        //Approach 1
-        /**
+    private void reversalAlgo(int[] nums, int k) {
         k=k%nums.length;
         if(k<0){
             k=k+nums.length;
         }
-        */
-        // Approach 2
-        int n = nums.length;
-        k = ((k % n) + n) % n;  //<--
-        reverse(nums,0,nums.length-1-k);
+        // Right Rotation
+        reverse (nums,0,nums.length-1-k);
         reverse(nums,nums.length-1-k+1,nums.length-1);
         reverse(nums,0,nums.length-1);
     }
 
+    // Left Rotation || (counter-clockwise direction)
+    private void reversalAlgo2(int[] nums, int k) {
+        k=k%nums.length;
+        if(k<0){
+            k=k+nums.length;
+        }
+        reverse (nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+        reverse(nums,0,nums.length-1);
+    }
+
     private void reverse(int[] arr, int i,int j){
-        while(i<=j){
+        while(i<j){
             int temp = arr[i];
             arr[i]=arr[j];
             arr[j]=temp;
