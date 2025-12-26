7faed05ea7f1977c3cfac24be2d941d7f7289ff3
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-26 23:52:14 +0530

    Time: 2 ms (93.38%), Space: 65 MB (72.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,9 +1,35 @@
 // LeetCode 238
 class Solution {
-    public int[] productExceptSelf(int[] nums) {    
-        return prefixAndSuffixApproach(nums);
+    public int[] productExceptSelf(int[] nums) {            
         //return bruteForceApproach(nums);
         //return byDivision(nums);
+        return prefixAndSuffixApproach(nums);
+    }
+
+    // Tuesday, March 18, 2025 8:04:59 PM
+    // Time Complexity:O(n) | Space Complexity:O(n)
+    private int[] prefixAndSuffixApproach(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        
+        // Base Case 
+        prefix[0]=1;
+        suffix[nums.length-1]=1;
+
+        // Start From 2 Location From Start
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+            
+        // Start from 2 Location From end
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        int[] ans = new int[nums.length];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=prefix[i]*suffix[i];  //<--
+        }
+        return ans;
     }
 
     // [-1,1,0,-3,3]
@@ -42,26 +68,5 @@ class Solution {
         return res;
     }
 
-    // Tuesday, March 18, 2025 8:04:59 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    private int[] prefixAndSuffixApproach(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] suffix = new int[nums.length];
-        prefix[0]=1;
-        // Start From 2 Location From Start
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
-        }
-
-        suffix[nums.length-1]=1;
-        // Start from 2 Location From end
-        for(int i=nums.length-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i+1];
-        }
-        int[] ans = new int[nums.length];
-        for(int i=0;i<ans.length;i++){
-            ans[i]=prefix[i]*suffix[i];
-        }
-        return ans;
-    }    
+        
 }
