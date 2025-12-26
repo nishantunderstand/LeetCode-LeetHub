59b64ab531dcd69592fe2d5c475b3f94046723f0
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-26 23:05:51 +0530

    Time: 21 ms (33.07%), Space: 92.2 MB (5.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,23 +1,25 @@
 // Leetcode : 287
-/**
-Here I need to return the Element , Not the Index
-1. Sorting , then compare
-    Then I need to Duplicate Case 
-2. HashMap , value count >1 , Much Better
-    You cannot use 2 Pass - NO
-    You need to solve in 1 Pass
-
-Floyd's Tortoise and Hare algorithm
-*/
 
 class Solution {
     public int findDuplicate(int[] nums) {        
-        return hashSetApproach(nums);
+        //return hashMapApproach(nums);
+        return hashsetApproach(nums);
+    }
+
+    private int hashsetApproach(int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        for(int i=0;i<=nums.length;i++){
+            if(seen.contains(nums[i])){
+                return nums[i];
+            }
+            seen.add(nums[i]);
+        }
+        return -1;
     }
 
     // Wednesday, May 14, 2025 9:15:41 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
-    public int hashSetApproach(int[] nums) {
+    public int hashMapApproach(int[] nums) {
         int n = nums.length;
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (Integer num : nums) {
@@ -25,6 +27,7 @@ class Solution {
                 return num;
             }
             hm.put(num, 1); // Why are we not checking getOrDefault ?
+            // Using getOrDefault(num, 0) > 0 would also work, but it's unnecessary in this context, since the map is only storing presence.
         }
         return -1;
     }
