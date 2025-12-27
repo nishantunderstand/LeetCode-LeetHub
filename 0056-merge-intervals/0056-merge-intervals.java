71566caed6815377a46ae339939d4a754cb644de
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2025-12-27 16:22:24 +0530

    Time: 8 ms (89.04%), Space: 48.9 MB (62.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,38 +1,20 @@
-/**
-Invalid Input Edge Cases
-
-s1,e1
-s2,e2
-
-Overlap - e1>s2  ==> s1,e2
-
-Assumptions : Intervals are sorted.
-If they are not sorted, Then you need to sort them.
-Do i need to sort ??
-*/
+// LeetCode : 56
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
         if(intervals==null ||n ==0){
             return new int[0][0];
         }
-
         Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
-
-        int row = intervals.length;
-        int col = intervals[0].length;
-
-        LinkedList<int[]> merged = new LinkedList<>();
-
-        int s1 = intervals[0][0];
-        int e1 = intervals[0][1];
-
         
+        List<int[]> merged = new ArrayList<>();
+        int s1 = intervals[0][0];
+        int e1 = intervals[0][1];        
         for(int i=1;i<n;i++){
             int s2 = intervals[i][0];
             int e2 = intervals[i][1];
-            if(e1>=s2){
-                e1 = Math.max(e1,e2);
+            if(s2<=e1){
+                e1 = Math.max(e1,e2);  //<--
             }else{
                 merged.add(new int[]{s1,e1});
                 s1 = s2;
@@ -41,6 +23,7 @@ class Solution {
         }
         // How to add Array in A LinkedList 
         merged.add(new int[]{s1,e1});
+        
         // Convert LinkedList to Array and return , You need to learn [[Anki]]
         return merged.toArray(new int[merged.size()][]);        
     }
