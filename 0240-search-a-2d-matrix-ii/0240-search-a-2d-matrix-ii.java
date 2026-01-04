7c15c83a0c837943cf41570a83ab514be4c5570a
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-04 20:38:29 +0530

    Time: 3 ms (86.31%), Space: 48.2 MB (43.15%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,19 +1,23 @@
 // Leetcode : 240
-
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return binarySearchApproach(matrix,target);        
+        return stairCaseSearch(matrix,target);        
     }
 
     // Thursday, April 17, 2025 10:57:13 PM
     // Time Complexity:O(m+n) | Space Complexity:O(1)
-    private boolean binarySearchApproach(int[][] mat,int k){
+    private boolean stairCaseSearch(int[][] mat,int k){
         int row = 0, col = mat[0].length-1;
-
         while(row<mat.length && col>=0){
-            if(mat[row][col]==k) return true;
-            else if(mat[row][col]>k) col--;
-            else row++;
+            if(mat[row][col]==k){
+                return true;
+            }
+            else if(mat[row][col]>k){
+                col--;
+            }
+            else if(mat[row][col]<k){
+                row++;
+            }            
         }
         return false;
     }
