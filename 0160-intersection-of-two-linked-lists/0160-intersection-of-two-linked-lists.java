f0d765ac6c3907306afef2972b8690a8d0a4cee0
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-11 18:19:50 +0530

    Time: 2 ms (44.97%), Space: 53.1 MB (12.58%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,9 +9,22 @@
  *     }
  * }
  */
+// LeetCode : 160
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return hashSetApproach(headA,headB);
+        //return hashSetApproach(headA,headB);
+        return twoPointerApproach(headA,headB);
+    }
+
+    private ListNode twoPointerApproach(ListNode head1, ListNode head2){
+        if(head1==null || head2 == null) return null;
+        ListNode p1 = head1;
+        ListNode p2 = head2;
+        while(p1!=p2){
+            p1 = (p1!=null)?p1.next:head2;
+            p2 = (p2!=null)?p2.next:head1;
+        }
+        return p1; //p2
     }
 
     // Wednesday, January 29, 2025 6:37:56 PM
