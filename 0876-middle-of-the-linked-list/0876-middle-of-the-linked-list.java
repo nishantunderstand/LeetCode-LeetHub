f82958b9a1ed0ee7d2e15009394568580d0d8ef2
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-12 23:04:51 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.34%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,25 @@
+// LeetCode : 876
+/** 
+Even Or Odd Length
+How to find 1st Or 2nd Middle
+2nd Middle : Default fast=head, slow=head;
+1st Middle : fast = head.next, slow = head;
+*/
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        return fastAndSlow(head);
+    }
+
+    // Friday, March 21, 2025 6:02:29 PM
+    // Time Complexity:O(n) | Space Complexity:O(1)
+    public ListNode fastAndSlow(ListNode head){
+        if(head==null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
