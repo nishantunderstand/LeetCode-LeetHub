747206e0cb2925d9fba7f7183733833b65e68beb
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-17 13:16:46 +0530

    Time: 0 ms (100%), Space: 44.3 MB (40.61%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,8 +3,34 @@ class Solution {
     public int findPeakElement(int[] nums) {
         //return binarySearch(nums);
         //return bruteForceSearch(nums);
-        return bruteForceSearchPeak(nums);
+        //return bruteForceSearchPeak(nums);
+        return binarySearchLatest(nums);
     }        
+
+    // Sunday, February 16, 2025 4:42:32 PM
+    // Time Complexity:O(logn) | Space Complexity:O(1)
+    public int binarySearchLatest(int[] nums) {
+        int n = nums.length;
+        if (nums == null || n == 0) {
+            return -1;
+        }
+        int lo = 0;
+        int hi = n - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            // Increasing Slope
+            if (nums[mid] < nums[mid + 1]) {
+                lo = mid + 1;
+            }
+            // Decreasing Slope 
+            else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
+
     // Sunday, February 16, 2025 5:50:04 PM
     // Time Complexity:O(logn) | Space Complexity:O(1) 
     public int bruteForceSearchPeak(int[] nums) {
@@ -35,28 +61,7 @@ class Solution {
         return lo; //<--
     }
 
-    // Sunday, February 16, 2025 4:42:32 PM
-    // Time Complexity:O(logn) | Space Complexity:O(1)
-    public int binarySearch(int[] nums) {
-        int n = nums.length;
-        if (nums == null || n == 0) {
-            return -1;
-        }
-        int lo = 0;
-        int hi = n - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            // Increasing Slope
-            if (nums[mid] < nums[mid + 1]) {
-                lo = mid + 1;
-            }
-            // Decreasing Slope 
-            else {
-                hi = mid;
-            }
-        }
-        return lo;
-    }
+    
 
     // Sunday, February 16, 2025 4:39:55 PM
     // Time Complexity:O(n) | Space Complexity:O(1)
