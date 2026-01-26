2d6353937c01619bbc48a34622a796b571c704a1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-26 14:22:25 +0530

    Time: 0 ms (100%), Space: 46.5 MB (45.09%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,21 +1,8 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 // Leetcode : 328
-
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         return reOrderOddEvenList(head);
     }
-
     // Sunday, April 27, 2025 1:54:05 PM
     // Time Complexity:O(n) | Space Complexity:O(1)
     public ListNode reOrderOddEvenList(ListNode head) {
@@ -25,26 +12,26 @@ class Solution {
         ListNode oddDummy = new ListNode(0);
         ListNode evenDummy = new ListNode(0);
 
-        ListNode odd = oddDummy;
-        ListNode even = evenDummy;
+        ListNode oddtail = oddDummy;
+        ListNode eventail = evenDummy;
 
         ListNode curr = head;
         int idx = 1;  //<--
 
         while (curr != null) {
             if (idx % 2 == 1) {
-                odd.next = curr;  //<--
-                odd = odd.next;
+                oddtail.next = curr;  //<--
+                oddtail = oddtail.next;
             } else {
-                even.next = curr;
-                even = even.next;
+                eventail.next = curr;
+                eventail = eventail.next;
             }
             idx++;
             curr = curr.next;
         }
 
-        even.next = null;
-        odd.next = evenDummy.next;
+        eventail.next = null;  //<--
+        oddtail.next = evenDummy.next;  //<--
         return oddDummy.next;
     }
 }
