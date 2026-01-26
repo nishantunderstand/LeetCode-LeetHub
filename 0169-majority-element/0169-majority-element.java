771b9c02172b6cc06eb6cb3381d3d3f2402feb01
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-01-26 18:21:20 +0530

    Time: 13 ms (29.89%), Space: 52.8 MB (78.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,19 @@
+// LeetCode : 169
 class Solution {
     public int majorityElement(int[] nums) {
-        return hashMapAppraoch(nums);
+        return hashMapAppraoch(nums);        
     }
-
     // Thursday, February 13, 2025 2:50:03 PM
     // Time Complexity:O(n) | Space Complexity:O(n)
     public int hashMapAppraoch(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
         int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        for(int i=0;i<len;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        int target = len / 2;
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) { // <--
-            if (entry.getValue() > target) { // <--
+        int target = len/2;   
+        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){ //<--
+            if(entry.getValue()>target){ //<--
                 return entry.getKey();
             }
         }
