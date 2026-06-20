62bb09afcf0849a92a0b17247b2356ab7b5db9e6
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:18:45 +0530

    Time: 2 ms (72.1%), Space: 44.9 MB (22.53%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,54 +1,27 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        return recursiveSol(n);
-    }
-
-    // Monday, November 18, 2024 7:02:48 PM
-    // T.C - O(2^2n) | S.C - O(n)
-    public List<String> recursiveSol(int n){
         List<String> res = new ArrayList<>();
-        if(n<=0) return res;
-        
-        //recursiveHelper(res,"",0,0,n); //<--
-        
-        recursiveHelper2(res,"",n,n); //<--
+        helper(res,n,"",0,0);
         return res;
     }
-    // Tuesday, November 19, 2024 11:34:32 AM
-    // T.C - O(2^2n) | S.C - O(n)
-    public void recursiveHelper2(List<String> res,String curr,int open,int close){
-        if(open==0 && close==0){
-            res.add(curr);
+
+    private void helper(List<String> ans, int n,String curr,int open , int close){
+        // Base Case
+        if(open==n && close ==n){
+            ans.add(curr);
             return;
         }
-        if(open>0){
-            recursiveHelper2(res,curr+"(",open-1,close);
-        }
-        if(close>open){ //<--
-            recursiveHelper2(res,curr+")",open,close-1);
-        }
-    }
-
-    public void recursiveHelper(List<String> res,String curr,int open,int close,int n){
-        // if(open>close) return; This Will LEAD TO WRONG RESULT.we mistakenly prune out valid recursive
-
-        /** Base Case V1
-        if(curr.length()==2*n){
-            res.add(curr);
-            return;
-        }
-        */
-
-        if(open==n && close==n){
-            res.add(curr);
-            return;
+        // Recursive Function, Pick (, )        
+        if(open<n){
+            helper(ans,n,curr+"(",open+1,close);
         }
         
-        if(open<n){ //<--
-            recursiveHelper(res,curr+"(",open+1,close,n);
-        }
         if(close<open){ //<--
-            recursiveHelper(res,curr+")",open,close+1,n);
+            helper(ans,n,curr+")",open,close+1);
         }
+        
     }
 }
+// Sunday, June 7, 2026 2:54:08 PM
+// TC : O(2^n) | SC : O(n)
+// LeetCode : 22
