c2fa3e5108d02223252b8773f5a0f0c92646fc74
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:09:19 +0530

    Time: 0 ms (100%), Space: 45 MB (44.51%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,28 +1,34 @@
+// Saturday, June 6, 2026 2:30:22 PM
+// TC : O(logn) | SC : O(1)
 // LeetCode : 81
 class Solution {
     public boolean search(int[] nums, int target) {
         int low = 0;
         int high = nums.length-1;
-        while(low<=high){
+        while(low<= high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target){
                 return true;
             }
-            if(nums[low]==nums[mid] && nums[mid]==nums[high]){  //<--
+
+            // left = right = mid 
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
                 low++;
                 high--;
+                continue;
             }
-            // low Sorted
-            else if(nums[low]<=nums[mid]){                
-                if(nums[low]<=target && target<nums[mid]){  //<--
+
+            // left Sorted              
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target < nums[mid]){
                     high = mid-1;
                 }else{
                     low = mid+1;
                 }
             }
-            // high Sorted
+            // right Sorted
             else{
-                if(nums[mid]<target && target <= nums[high]){  //<--
+                if(nums[mid]< target && target<=nums[high]){
                     low = mid+1;
                 }else{
                     high = mid-1;
