e0ef853cb26ce335914d99b8d94ff2e732efaf37
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:19:09 +0530

    Time: 3 ms (34.29%), Space: 47.1 MB (29.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,12 @@
-// LeetCode 17
-
 class Solution {
-    /**
-    // If you plan to declare here that's how you will declare
-    private static final HashMap<Character,String> hm = new HashMap<>();
-    static{
+    Map<Character,String> hm;
+    public List<String> letterCombinations(String digits) {
+
+        if(digits==null || digits.length()==0){
+            return new ArrayList<>();
+        }
+
+        hm = new HashMap<>();        
         hm.put('0',"0");
         hm.put('1',"1");
         hm.put('2',"abc");
@@ -15,50 +17,38 @@ class Solution {
         hm.put('7',"pqrs");
         hm.put('8',"tuv");
         hm.put('9',"wxyz");
+
+        
+        return helper(digits);
         
     }
 
-    */
-
-    public List<String> letterCombinations(String digits) {
-        return hashingBackting(digits);
-    }
-
-    // Tuesday, February 11, 2025 4:35:02 PM
-    // Time Complexity:O(4^n) | Space Complexity:O(n)
-    public List<String> hashingBackting(String digits) {
-        List<String> res = new ArrayList<>();
-        if(digits==null || digits.isEmpty()) return res;
-
-        HashMap<Character,String> hm = new HashMap<>();
-        hm.put('0',"0");
-        hm.put('1',"1");
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
-
-        backtracking(res,new StringBuilder(),digits,0,hm);
-        return res;
-    }
-    
-    public void backtracking(List<String> res,StringBuilder curr,String digits,int index,HashMap<Character,String> hm){
-        if(index == digits.length()){
-            res.add(curr.toString());
-            return;
+    private List<String> helper(String digits){
+        // Base Case
+        if(digits.length()==0){
+            List<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
         }
-        char currDigit = digits.charAt(index);
-        String letters = hm.get(currDigit);
-        if(letters==null) return;
 
-        for(char ch : letters.toCharArray()){
-            curr.append(ch);
-            backtracking(res,curr,digits,index+1,hm);
-            curr.deleteCharAt(curr.length()-1); //<--
-        }                
+        char ch = digits.charAt(0);
+        String ros = digits.substring(1);
+        
+        // Faith 
+        List<String> rres = helper(ros); //<--
+        
+        // Self
+        List<String> mres = new ArrayList<>();
+        String code = hm.get(ch);
+
+        for(char c : code.toCharArray()){
+            for(String str : rres){
+                mres.add(c+str);
+            }
+        }
+        return mres;
     }
 }
+// Monday, June 8, 2026 8:35:45 PM
+// TC : O(2^n) | SC : O(n)
+// LeetCode : 17
