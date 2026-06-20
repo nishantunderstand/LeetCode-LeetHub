4c3c0ceec39f05e823fb4714fe747e24b7bd6466
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 17:35:30 +0530

    Time: 0 ms (100%), Space: 45.4 MB (68.24%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -1,89 +1,15 @@
-// Leetcode : 83
+// LeetCode : 83
+// Dry Run
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        //return sortedCase(head);
-        //return unsortedCase(head);
-        //return unsortedCaseDummy(head);
-        return hashSetApproach(head);
-    }
-
-    // Friday, May 16, 2025 1:25:33 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    private ListNode hashSetApproach(ListNode head){
-        if (head == null) return null;  //<--
-        HashSet<Integer> seen = new HashSet<>();
-        ListNode curr = head;
-        seen.add(curr.val);
-
-        while(curr.next!=null){
-            if(seen.contains(curr.next.val)){
-                curr.next = curr.next.next;
-            }else{
-                seen.add(curr.next.val); //<--
-                curr = curr.next;
-            }            
-        }
-        return head;
-    }
-
-    // Friday, May 16, 2025 1:25:33 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    public ListNode unsortedCaseDummy(ListNode head) {
-        HashSet<Integer> seen = new HashSet<>();
-        ListNode dummy = new ListNode(0, head); // Dummy before head
-        ListNode prev = dummy, curr = head;
-
-        while (curr != null) {
-            if (seen.add(curr.val)) { // First time seeing value
-                prev = curr;
-            } else {
-                prev.next = curr.next; // Skip duplicate
-            }
+       ListNode curr = head;
+       while(curr!=null && curr.next!=null){
+        if(curr.val==curr.next.val){
+            curr.next = curr.next.next;
+        }else{
             curr = curr.next;
         }
-        return dummy.next;
-    }        
-
-    // Friday, May 16, 2025 1:25:33 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-     private ListNode unsortedCase(ListNode head) {
-        if (head == null) return null;
-        HashSet<Integer> seen = new HashSet<>();
-        ListNode curr = head;
-        ListNode prev = null;
-
-        while (curr != null) {
-            if (seen.contains(curr.val)) {
-                prev.next = curr.next; // Remove curr
-            } else {
-                seen.add(curr.val);
-                prev = curr; // Move prev to curr
-            }
-            curr = curr.next; // Move curr always
-        }
-
-        return head;
-    }
-
-    // Saturday, April 26, 2025 8:36:11 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
-    private ListNode sortedCase(ListNode head){
-        if(head==null || head.next==null) return head;        
-        ListNode curr = head;
-        while(curr!=null && curr.next!=null){
-            if(curr.val == curr.next.val){
-                // Skip Duplicate
-                curr.next = curr.next.next; 
-            }else{
-                // Move to Next Node
-                curr = curr.next; 
-            }
-        }
-        // return curr; <-- WRONG
-        return head;        
+       } 
+       return head;
     }
 }
-/** 
-    When to use dummyNode Concept ?
-    When Head can be deleted
-*/
