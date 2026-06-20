85761557c4b297d018ec5448614a73a4c8ab8f8b
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 17:49:14 +0530

    Time: 0 ms (100%), Space: 46.9 MB (15.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,53 +9,17 @@
  *     }
  * }
  */
-
-// Leetcode : 141
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        //return hashMapApproach(head);        
-        //return hashSetApproach(head);  
-        return fastAndSlow(head);    
-    }
-
-    // Thursday, May 15, 2025 4:38:55 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
-    public boolean fastAndSlow(ListNode head){
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
-            fast = fast.next.next;
             slow = slow.next;
-            if(fast==slow){
+            fast = fast.next.next;
+            if(slow==fast){
                 return true;
             }
-        }
-        return false;
-    }
-
-    // Monday, November 18, 2024 9:27:32 PM
-    // T.C - O(n) | S.C - O(n/)
-    public boolean hashSetApproach(ListNode head){
-        if(head==null) return false;
-        HashSet<ListNode> hs = new HashSet<>();
-        while(head!=null){
-            if(hs.contains(head)) return true;
-            hs.add(head);
-            head = head.next;
-        }
-        return false;
-    }
-
-    // Monday, November 18, 2024 9:27:38 PM
-    // T.C - O(n) | S.C - O(n)
-    public boolean hashMapApproach(ListNode head){
-        if(head==null) return false;
-        HashMap<ListNode,Integer> hm = new HashMap<>();
-        while(head!=null){
-            if(hm.containsKey(head)) return true;
-            hm.put(head,1);
-            head = head.next;
-        }
+        }       
         return false;
     }
 }
