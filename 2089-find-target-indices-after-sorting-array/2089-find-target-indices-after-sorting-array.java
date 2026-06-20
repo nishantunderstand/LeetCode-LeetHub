e1c4a33d259a086e1161d7cbe2941d3f77bb3204
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:08:08 +0530

    Time: 5 ms (73.6%), Space: 50.6 MB (9.93%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,35 +1,20 @@
-// Leetcode : 2089
-class Solution {
+class Solution {    
     public List<Integer> targetIndices(int[] nums, int target) {        
-        //return bySorting(nums,target);
-        
-        Arrays.sort(nums);  //<--
-        List<Integer> res = new ArrayList<>();
-        recursiveApproach(nums,target,0,res);
-        return res;
-    }
-
-    // Sunday, November 23, 2025 1:45:18 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    private void recursiveApproach(int[] nums, int target, int index, List<Integer> res){
-        if(index == nums.length) return;
-        if(nums[index]==target) res.add(index);
-        recursiveApproach(nums,target,index+1,res);
-
-    }
-
-    // Friday, May 16, 2025 4:58:23 PM
-    // Time Complexity:O(nlogn) | Space Complexity:O(n)
-    public List<Integer> bySorting(int[] nums, int target) {
-        List<Integer> res = new ArrayList<>();
-        if(nums==null || nums.length==0) return res;        
-        
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                res.add(i);
-            }
+        List<Integer> res = new ArrayList<>();
+        helper(nums,target,0, res);
+        return res;        
+    }
+    private void helper(int[] nums, int data, int idx,List<Integer> res){
+        if(idx==nums.length){
+            return;
         }
-        return res;
+        if(nums[idx]==data){            
+            res.add(idx);            
+        }
+        helper(nums,data,idx+1,res);        
     }
 }
+// Saturday, May 30, 2026 7:45:04 PM
+// TC : O(n) | SC : O(1)
+// LeetCode : 2089
