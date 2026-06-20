637575a08c4f8240e0951bf9bd347b04a09229d1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:20:34 +0530

    Time: 7 ms (64.41%), Space: 44.4 MB (47.88%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,33 +1,11 @@
-// LeetCode 151
 class Solution {
-    public String reverseWords(String s) {    
-        //return stringInBuildMethod(s);
-        return reverseWordsStringBuilder(s);
-    }
-
-    private String reverseWordsStringBuilder(String str){
-        String[] words = str.trim().split("\\s+");
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
         StringBuilder sb = new StringBuilder();
-        for(int i=words.length-1,i>=0;i--){
-            sb.append(words[i]);
-            if(i>0){
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
+        for(int i=arr.length-1;i>=0;i--){
+            sb.append(arr[i]);
+            if(i>0) sb.append(" ");
+        }               
+        return new String(sb);
     }
-
-    // Thursday, March 20, 2025 6:11:18 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    private String stringInBuildMethod(String s) {
-        // Trim leading/trailing spaces and split by one or more spaces
-        String[] input = s.trim().split("\\s+");
-        StringBuilder rev = new StringBuilder();
-        // Iterate From Reverse Order
-        for(int i=input.length-1;i>=0;i--){ //<--
-            rev.append(input[i]);
-            if(i>0) rev.append(" "); // Add Space Between Word //<--
-        }
-        return rev.toString();
-    }    
 }
