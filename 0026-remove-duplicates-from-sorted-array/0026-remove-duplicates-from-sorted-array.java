5ddb7c69a4758defe517b85ce2e0d78228e74264
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:21:41 +0530

    Time: 0 ms (100%), Space: 46.6 MB (80.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,23 +1,12 @@
-// Leetcode : 26
 class Solution {
     public int removeDuplicates(int[] nums) {
-        return inplaceApproach(nums);
-            
-    }
-
-    // Wednesday, April 23, 2025 6:32:02 PM
-    // Time Complexity:O(n) | Space Complexity:O(1)
-    public int inplaceApproach(int[] nums) {
-        if (nums.length <= 0) {
-            return 0;
+        int j = 0;        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[j]){                
+                j++;
+                nums[j]=nums[i];
+            }            
         }
-        int idx = 1;  //<--
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i-1]) {                
-                nums[idx] = nums[i];
-                idx++  //<--
-            }
-        }
-        return idx;
+        return j+1;
     }
 }
