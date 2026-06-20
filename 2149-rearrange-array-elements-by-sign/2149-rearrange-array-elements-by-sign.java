c5ce9de3419994930f70d7630a08625ce1453429
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 18:20:16 +0530

    Time: 3 ms (100%), Space: 175.5 MB (56.42%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int pos = 0;
-        int odd = 1;
-        while(pos<nums.length && odd <nums.length){
-            if(nums[pos]>0) pos+=2;
-            else if(nums[neg]<0) neg+=2;
-            else{
-                swap(nums,pos,neg);
-                pos+=2;
-                neg+=2;
-            }
-        }
-        return nums;        
-    }
+        int n = nums.length;
+        int posIdx=0, negIdx = 1;
+        int[] res = new int[n];
 
-    
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                res[posIdx] = nums[i];
+                posIdx+=2;
+            }else{
+                res[negIdx] = nums[i];
+                negIdx+=2;
+            }
+        }  
+
+        return res;
+    }
 }
-// Sunday, June 14, 2026 1:18:28 AM
-// TC : O(2^nnlognk) | SC : O(2^n1logkh)
-// LeetCode : 2149
