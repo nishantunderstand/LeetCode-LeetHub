58a408212505da859abd2080f6ca7460640c4cbd
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-20 17:49:23 +0530

    Time: 0 ms (100%), Space: 44.1 MB (73.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,58 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Saturday, May 16, 2026 12:44:56 AM
 // LeetCode : 21
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //return iterativeMerge(list1,list2);
-        return recursiveMerge(list1,list2);
-    }
-
-    // Saturday, February 8, 2025 8:05:38 PM
-    // Time Complexity:O(n) | Space Complexity:O(n)
-    public ListNode recursiveMerge(ListNode list1, ListNode list2) {
-        if(list1==null) return list2;
-        if(list2==null) return list1;
-            if(list1.val<list2.val){
-                list1.next=recursiveMerge(list1.next,list2); //<--
-                return list1; //<--
-            }else{
-                list2.next=recursiveMerge(list1,list2.next); //<--
-                return list2; //<--
-            }
-    }
-
-    // Saturday, December 14, 2024 6:50:41 PM
-    // T.C - O(n+m) | S.C - O(1)
-    public ListNode iterativeMerge(ListNode list1, ListNode list2) {
         if(list1==null) return list2;
         if(list2==null) return list1;
 
-        ListNode dummyNode = new ListNode(-1); 
-        ListNode current = dummyNode; //<--
+        ListNode dummyNode = new ListNode(-1);
+        ListNode curr = dummyNode;
         
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
-                current.next=list1; 
-                list1=list1.next;           
+                curr.next = list1;
+                list1 = list1.next;
+                curr = curr.next;
             }else{
-                current.next=list2;
-                list2=list2.next;
+                curr.next = list2;
+                list2 = list2.next;
+                curr = curr.next;
             }
-            current=current.next;
         }
-        /**
-        if(list1!=null){current.next=list1;}
-        if(list2!=null){current.next=list2;}
-        */
-        current.next=(list1!=null)?list1:list2;
-        return dummyNode.next; //<--
+
+        if(list1!=null) curr.next = list1;
+        if(list2!=null) curr.next = list2;
+        return dummyNode.next;
     }
 }
