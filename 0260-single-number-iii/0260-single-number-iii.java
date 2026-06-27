dd7a6c4dad5683d346cd4f0e12f521fdbd746ed1
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-27 20:31:15 +0530

    Time: 7 ms (29.62%), Space: 47.9 MB (59.55%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,6 +1,35 @@
 // Leetcode : 260
 class Solution {
-    public int[] singleNumber(int[] nums) {
+    public int[] singleNumber(int[] nums) {        
+        return hashMap(nums);
+        //return bits(nums);
+    }
+
+    // Saturday, June 27, 2026 8:30:06 PM
+	// TC : O(n) | SC : O(1)
+    private int[] bits(int[] nums){
+        int xor = 0;
+        for(int num :  nums){
+            xor ^=num;
+        }
+        
+        int rmsb = (xor & (xor-1)) ^ xor; // xor & -xor;
+        int b1 = 0; int b2 = 0;
+
+        for(int num : nums){
+            if((rmsb & num)==0){
+                b1^=num;
+            }else{
+                b2^=num;
+            }
+        }
+        return new int[]{b1,b2};
+    }
+
+
+    // Saturday, June 27, 2026 8:25:51 PM
+	// TC : O(n) | SC : O(n)
+    public int[] hashMap(int[] nums) {
         int n = nums.length;
         if (nums == null || nums.length == 0) {
             return new int[0];
@@ -9,14 +38,17 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
         }
+        
         ArrayList<Integer> res = new ArrayList<>();
         // As we are viewing No Data Modification.
         // If you try to modify Errror, Try Iterator<Integer> it = hm.keySet().iterator();
+        
         for (int ikey : hm.keySet()) {
             if (hm.get(ikey) == 1) {
                 res.add(ikey);
             }
         }
+        
         int[] resArr = new int[res.size()];
         for (int i = 0; i < resArr.length; i++) {
             resArr[i] = res.get(i);
