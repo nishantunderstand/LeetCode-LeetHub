853abf51fe3365c7c66cb6deaa8a238b489eacac
Nishant Raj <90112486+nishantunderstand@users.noreply.github.com>
2026-06-28 19:27:48 +0530

    Time: 49 ms (58.13%), Space: 50.4 MB (13.35%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,32 @@
+// LeetCode : 992
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+
+    private int atMostK(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int startIdx = 0;
+        int cnt = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            // Same as Calculating WindowSum
+            int num = nums[i];
+            count.put(num, count.getOrDefault(num, 0) + 1);
+
+            while (count.size() > k) {
+                // Same as Decreasing WindowSum
+                int startIdxNum = nums[startIdx];
+                count.put(startIdxNum, count.get(startIdxNum) - 1);
+                
+                // If it reach Zero, Then Discard It
+                if (count.get(startIdxNum) == 0) {
+                    count.remove(startIdxNum);
+                }
+                startIdx++;
+            }
+            cnt += i - startIdx + 1;
+        }
+        return cnt;
+    }
+}
